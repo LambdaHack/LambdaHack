0724e2a3af8cfdc7994336d367d3d385a82e7604
andres <andres@cs.uu.nl>
2008-01-31 03:56:36 +0200

    colors, at the cost of speed

--- Display/Gtk.hs
+++ Display/Gtk.hs
@@ -2,6 +2,7 @@ module Display.Gtk
   (startup, shutdown,
    display, nextEvent, setBG, setFG, Session, blue, magenta, attr) where
 
+import Control.Monad
 import Control.Concurrent
 import Graphics.UI.Gtk hiding (Attr)
 import Data.List as L
@@ -11,6 +12,7 @@ import Level
 data Session =
   Session {
     schan :: Chan String,
+    stag  :: TextTag,
     sbuf  :: TextBuffer }
 
 startup :: (Session -> IO ()) -> IO ()
@@ -23,9 +25,11 @@ startup k =
     tt <- textTagNew Nothing
     ttt <- textTagTableNew
     textTagTableAdd ttt tt
+    set tt [ textTagBackground := "#FF0000" ]
 
     -- text buffer
     tb <- textBufferNew (Just ttt)
+    textBufferSetText tb (unlines (replicate 25 (replicate 80 ' ')))
 
     -- create text view
     tv <- textViewNewWithBuffer tb
@@ -43,7 +47,7 @@ startup k =
     widgetModifyText tv StateNormal white
 
     ec <- newChan 
-    forkIO $ k (Session ec tb)
+    forkIO $ k (Session ec tt tb)
     
     onKeyPress tv (\ e -> writeChan ec (eventKeyName e) >> yield >> return True)
 
@@ -57,18 +61,43 @@ shutdown _ = mainQuit
 
 display :: Area -> Session -> (Loc -> (Attr, Char)) -> String -> IO ()
 display ((y0,x0),(y1,x1)) session f status =
+{-
   let img = unlines $
             L.map (L.map (\ (x,y) -> let (a,c) = f (y,x) in c))
             [ [ (x,y) | x <- [x0..x1] ] | y <- [y0..y1] ]
   in  textBufferSetText (sbuf session) (img ++ status)
+-}
+  do
+    ttt <- textBufferGetTagTable (sbuf session)
+    tb <- textBufferNew (Just ttt)
+    sequence_ [ setTo tb (stag session) (y,x) c a | y <- [y0..y1], x <- [x0..x1], let loc = (y,x), let (a,c) = f (y,x) ]
+    textBufferSetText (sbuf session) ""
+    iter <- textBufferGetEndIter (sbuf session)
+    is <- textBufferGetStartIter tb
+    ie <- textBufferGetEndIter tb
+    textBufferInsertRange (sbuf session) iter is ie
+
+setTo :: TextBuffer -> TextTag -> Loc -> Char -> Bool -> IO ()
+setTo tb tt (ly,lx) x blue =
+  do
+    iter <- textBufferGetEndIter tb
+    when (lx == 0 && ly /= 0) $ textBufferInsert tb iter "\n"
+    iter <- textBufferGetEndIter tb
+    textBufferInsert tb iter [x]
+    when blue $
+      do
+        ie <- textBufferGetEndIter tb
+        ib <- textIterCopy ie
+        textIterBackwardChar ib
+        textBufferApplyTag tb tt ib ie
 
 nextEvent :: Session -> IO String
 nextEvent session = readChan (schan session)
 
 setBG   = id
 setFG   = id
-blue    = id
+blue    = const True
 magenta = id
-attr    = ()
+attr    = False
 
-type Attr = ()
+type Attr = Bool
