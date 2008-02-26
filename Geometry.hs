43367e0a3b106159fa531acb8824533760640ae0
andres <andres@cs.uu.nl>
2008-02-26 16:02:55 +0200

    added towards function

--- Geometry.hs
+++ Geometry.hs
@@ -10,6 +10,19 @@ type Loc = (Y,X)
 type Dir = (Y,X)
 type Area = ((Y,X),(Y,X))
 
+towards :: (Loc,Loc) -> Dir
+towards ((y0,x0),(y1,x1)) =
+  let dy = y1 - y0
+      dx = x1 - x0
+      angle = atan (fromIntegral dy / fromIntegral dx) / (pi / 2)
+      dir | angle <= -0.75 = (-1,0)
+          | angle <= -0.25 = (-1,1)
+          | angle <= 0.25  = (0,1)
+          | angle <= 0.75  = (1,1)
+          | angle <= 1.25  = (1,0)
+          | otherwise      = (0,0)
+  in  if dx >= 0 then dir else neg dir
+
 distance :: (Loc,Loc) -> Int
 distance ((y0,x0),(y1,x1)) = (y1 - y0)^2 + (x1 - x0)^2
 
