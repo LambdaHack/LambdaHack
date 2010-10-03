9d302d4cb3a298c30cb5678940da40417c3ef4da
Mikolaj <mikolaj.konarski@gmail.com>
2010-10-04 04:26:41 +0800

    [dependencies] adapted to vty 4.4
    No crossed over characters on my machine now with 'cabal install -fvty'.
    Curses backend unchanged but works OK with newest hscurses with
    'cabal install -fcurses'.

--- src/Display/Vty.hs
+++ src/Display/Vty.hs
@@ -19,14 +19,14 @@ startup k = V.mkVty >>= k
 
 display :: Area -> Session -> (Loc -> (Attr, Char)) -> String -> String -> IO ()
 display ((y0,x0),(y1,x1)) vty f msg status =
-    let img = (foldr (<->) V.empty . 
-               L.map (foldr (<|>) V.empty . 
-                      L.map (\ (x,y) -> let (a,c) = f (y,x) in renderChar a c)))
+    let img = (foldr (<->) V.empty_image .
+               L.map (foldr (<|>) V.empty_image .
+                      L.map (\ (x,y) -> let (a,c) = f (y,x) in char a c)))
               [ [ (x,y) | x <- [x0..x1] ] | y <- [y0..y1] ]
-    in  V.update vty (Pic NoCursor 
-         (renderBS attr (BS.pack (L.map (fromIntegral . ord) (toWidth (x1 - x0 + 1) msg))) <->
+    in  V.update vty (pic_for_image
+         (utf8_bytestring attr (BS.pack (L.map (fromIntegral . ord) (toWidth (x1 - x0 + 1) msg))) <->
           img <-> 
-          renderBS attr (BS.pack (L.map (fromIntegral . ord) (toWidth (x1 - x0 + 1) status)))))
+          utf8_bytestring attr (BS.pack (L.map (fromIntegral . ord) (toWidth (x1 - x0 + 1) status)))))
 
 toWidth :: Int -> String -> String
 toWidth n x = take n (x ++ repeat ' ')
@@ -34,7 +34,7 @@ toWidth n x = take n (x ++ repeat ' ')
 nextEvent :: Session -> IO String
 nextEvent session =
   do
-    e <- V.getEvent session
+    e <- V.next_event session
     case e of
       V.EvKey (KASCII '<') [] -> return "less"
       V.EvKey (KASCII '>') [] -> return "greater"
@@ -48,3 +48,11 @@ nextEvent session =
       V.EvKey KEsc []         -> return "Escape"
       V.EvKey KEnter []       -> return "Return"
       _                       -> nextEvent session
+
+attr = def_attr
+
+setBold a = with_style a bold
+
+setBG c a = with_back_color a c
+
+setFG c a = with_fore_color a c
