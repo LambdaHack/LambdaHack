91c533a2d74a49861e6e8ebb6ba0167618f95243
Mikolaj <mikolaj.konarski@gmail.com>
2010-11-06 01:03:31 +0100

    the space key no longer skips turn
    That was dangerous, because space is also the 'show next part of the message'
    key, so players may press it often just to make sure there is no more
    messages to display.

--- src/Turn.hs
+++ src/Turn.hs
@@ -167,7 +167,6 @@ handle session (state@(State { splayer = player@(Monster { mhp = php, mdir = pdi
                            "Escape"  -> displayCurrent "Press Q to quit." Nothing >> h
 
                            -- wait
-                           "space"   -> loop session nstate ""
                            "period"  -> loop session nstate ""
 
                            -- look
