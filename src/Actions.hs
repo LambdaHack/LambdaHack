2fe2852cfcd0309cb397f0062beaff10e5089d34
Mikolaj <mikolaj.konarski@gmail.com>
2011-08-04 23:54:28 +0200

    revert a part of "abort some actions with an empty message"
    This reverts q part of commit 3cd423e9fa120519f69319359ff35a02f4b8a3c1
    concerning running, because the message for the last move of running
    was being lost.

--- src/Actions.hs
+++ src/Actions.hs
@@ -184,7 +184,7 @@ continueRun dir =
         exit _            = False
     let hop t
           | monstersVisible || heroThere
-            || newsReported || itemsHere || exit t = abortWith msg
+            || newsReported || itemsHere || exit t = abort
         hop Corridor =
           -- in corridors, explore all corners and stop at all crossings
           -- TODO: even in corridors, stop if you run past an exit (rare)
@@ -199,16 +199,16 @@ continueRun dir =
                   case L.filter (\ x -> not $ diagonal x) ns of
                     [ortoDir]
                       | allCloseTo ortoDir -> run ortoDir
-                    _ -> abortWith msg
+                    _ -> abort
         hop _  -- outside corridors, never change direction
-          | not dirOK = abortWith msg
+          | not dirOK = abort
         hop _         =
           let ns = L.filter (\ x -> x /= dir && distance (neg dir, x) > 1) moves
               ls = L.map (loc `shift`) ns
               as = L.filter (\ x -> accessible lmap loc x
                                     || openable 1 lmap x) ls
               ts = L.map (tterrain . (lmap `at`)) as
-          in  if L.any exit ts then abortWith msg else run dir
+          in  if L.any exit ts then abort else run dir
     hop (tterrain t)
 
 ifRunning :: (Dir -> Action a) -> Action a -> Action a
