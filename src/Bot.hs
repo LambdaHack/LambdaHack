1af39c63888433c2284baf43d8e20d09f9cd5937
Mikolaj <mikolaj.konarski@gmail.com>
2011-08-08 21:45:47 +0200

    let the bot generate random letters
    To be used as in
      Bot 42 | Allure > /tmp/log

--- src/Bot.hs
+++ src/Bot.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import qualified System.Random as R
+import System.Environment
+
+move :: R.StdGen -> IO ()
+move g = do
+  let (c, ng) = R.randomR ('A', 'z') g
+  putChar c
+  move ng
+
 main :: IO ()
-main = return ()
+main = do
+  args <- getArgs
+  let seed = read (head args)
+  move (R.mkStdGen seed)
