66690d4fb5f9b01bccac4dda536ba05b107b374f
Mikolaj <mikolaj.konarski@gmail.com>
2011-08-09 00:56:44 +0200

    limit the number of commands the bot generates before stopping
    To be used as in
      Bot 3 300000 | Allure > /tmp/log

--- src/Bot.hs
+++ src/Bot.hs
@@ -2,15 +2,17 @@ module Main where
 
 import qualified System.Random as R
 import System.Environment
+import Control.Monad
 
-move :: R.StdGen -> IO ()
-move g = do
-  let (c, ng) = R.randomR ('A', 'z') g
+move :: R.StdGen -> Int -> IO ()
+move g k = do
+  let (c, ng) = R.randomR ('>', 'z') g
   putChar c
-  move ng
+  when (k > 0) $ move ng (k - 1)
 
 main :: IO ()
 main = do
   args <- getArgs
-  let seed = read (head args)
-  move (R.mkStdGen seed)
+  let seed  = read (head args)
+      count = read (head (tail args))
+  move (R.mkStdGen seed) count
