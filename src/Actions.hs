0659d105c64129987707b791ad721344844a41fa
Mikolaj <mikolaj.konarski@gmail.com>
2011-09-11 02:01:50 +0200

    Fix running past closed doors in a corridor

--- src/Actions.hs
+++ src/Actions.hs
@@ -190,7 +190,8 @@ continueRun dir =
           -- in corridors, explore all corners and stop at all crossings
           -- TODO: even in corridors, stop if you run past an exit (rare)
           let ns = L.filter (\ x -> distance (neg dir, x) > 1
-                                    && accessible lm loc (loc `shift` x))
+                                    && (accessible lm loc (loc `shift` x))
+                                        || openable 1 lm (loc `shift` x))
                             moves
               allCloseTo main = L.all (\ d -> distance (main, d) <= 1) ns
           in  case ns of
