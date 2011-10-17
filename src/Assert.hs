89ca14e71422ff9389a65ad4779a2dc496bdcfc9
Mikolaj <mikolaj.konarski@gmail.com>
2011-10-17 21:31:37 +0200

    Record a failed experiment to inline asserts
    Failed, because the source location I get is from the call site, after all.

--- src/Assert.hs
+++ src/Assert.hs
@@ -4,11 +4,34 @@ import Control.Exception (assert)
 import System.IO
 import System.IO.Unsafe
 
+-- | If the condition fails, Display the value blamed for the failure.
+-- Used as in "assert (c /= 0 `blame` c) $ 10 / c".
 blame :: Show a => Bool -> a -> Bool
-blame condition culprit
+blame condition blamed
   | condition = True
   | otherwise =
     let action = do
-          hPutStrLn stderr "Internal error, possibly due to the following:"
-          hPutStrLn stderr ("  " ++ show culprit)
+          hPutStrLn stderr
+            "Internal error occurred and the following is to blame:"
+          hPutStrLn stderr ("  " ++ show blamed)
     in unsafePerformIO action `seq` False
+
+-- | Assert that displays the value to blame for the failure.
+-- The INLINE pragme serves to show the source location of the call site,
+-- not of this definition. Unfortunately, it does not work.
+assertB :: Show a => Bool -> a -> b -> b
+{-# INLINE assertB #-}
+assertB condition blamed rest =
+  assert (condition `blame` blamed) rest
+
+-- | Like Prelude.undefined, but shows the source location.
+-- Except that it does not work.
+undef :: b
+{-# INLINE undef #-}
+undef = assert False (error "Internal error inside Assert.undef")
+
+-- | Like undef, but also displays the value to blame for the failure.
+undefB :: Show a => a -> b
+{-# INLINE undefB #-}
+undefB blamed =
+  assert (False `blame` blamed) (error "Internal error inside Assert.undefB")
