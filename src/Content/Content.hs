88664c63493d7d1abce778f84e6c28cbe34f2382
Mikolaj <mikolaj.konarski@gmail.com>
2011-10-30 17:37:42 +0100

    Content indices are now Word8
    256 indices should be enough for everybody

--- src/Content/Content.hs
+++ src/Content/Content.hs
@@ -2,14 +2,15 @@ module Content.Content (Content(..)) where
 
 import qualified Data.List as L
 import qualified Data.IntMap as IM
+import qualified Data.Word as Word
 
 class Content a where
   getFreq :: a -> Int
 
   content :: [a]
 
-  kindAssocs :: [(Int, a)]
+  kindAssocs :: [(Word.Word8, a)]
   kindAssocs = L.zip [0..] content
 
   kindMap :: IM.IntMap a
-  kindMap = IM.fromDistinctAscList kindAssocs
+  kindMap = IM.fromDistinctAscList $ L.zip [0..] content
--- src/Kind.hs
+++ src/Kind.hs
@@ -5,20 +5,21 @@ module Kind
 import Data.Binary
 import qualified Data.List as L
 import qualified Data.IntMap as IM
+import qualified Data.Word as Word
 
 import Utils.Assert
 import Content.Content
 import Frequency
 
-newtype Id a = Id Int
+newtype Id a = Id Word.Word8
   deriving (Show, Eq, Ord)
 
 instance Binary (Id a) where
-  put (Id i) = putWord8 $ fromIntegral i
-  get = fmap (Id . fromIntegral) getWord8
+  put (Id i) = putWord8 i
+  get = fmap Id getWord8
 
 getKind :: Content a => Id a -> a
-getKind (Id i) = kindMap IM.! i
+getKind (Id i) = kindMap IM.! (fromIntegral i)
 
 getId :: Content a => (a -> Bool) -> Id a
 getId f = case [Id i | (i, k) <- kindAssocs, f k] of
@@ -29,4 +30,4 @@ frequency :: Content a => Frequency (Id a, a)
 frequency = Frequency [(getFreq k, (Id i, k)) | (i, k) <- kindAssocs]
 
 foldrWithKey :: Content a => (Id a -> a -> b -> b) -> b -> b
-foldrWithKey f z = L.foldr (\ (k, a) -> f (Id k) a) z kindAssocs
+foldrWithKey f z = L.foldr (\ (i, a) -> f (Id i) a) z kindAssocs
