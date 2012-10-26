e420c4cd27d2ff4ab5c446907cea1f7147617072
Mikolaj <mikolaj.konarski@gmail.com>
2012-10-26 14:55:29 +0200

    Separate Frontend and Display
    Some common code will be moved from frontends to Display.hs,
    in particular the combat animations drawn via timed threads
    will be ported from gtk to there.

--- Game/LambdaHack/Display.hs
+++ Game/LambdaHack/Display.hs
@@ -1,6 +1,5 @@
 -- | Display game data on the screen using one of the available frontends
--- (determined at compile time with cabal flags).
-{-# LANGUAGE CPP #-}
+-- and derived display operations.
 module Game.LambdaHack.Display
   ( -- * Re-exported frontend
     FrontendSession, startup, frontendName, nextEvent, promptGetKey
@@ -8,18 +7,7 @@ module Game.LambdaHack.Display
   , displayFrame
   ) where
 
--- Wrapper for selected Display frontend.
-
-#ifdef CURSES
-import Game.LambdaHack.Display.Curses as D
-#elif VTY
-import Game.LambdaHack.Display.Vty as D
-#elif STD
-import Game.LambdaHack.Display.Std as D
-#else
-import Game.LambdaHack.Display.Gtk as D
-#endif
-
+import Game.LambdaHack.Frontend
 import qualified Game.LambdaHack.Color as Color
 
 -- | Push a frame or a single frame's worth of delay to the frame queue.
---
+++ Game/LambdaHack/Frontend.hs
@@ -0,0 +1,19 @@
+-- | Re-export the display operations of the chosen frontend
+-- (determined at compile time with cabal flags).
+{-# LANGUAGE CPP #-}
+module Game.LambdaHack.Frontend
+  ( -- * Re-exported frontend
+    FrontendSession, startup, frontendName, nextEvent, promptGetKey, display
+  ) where
+
+-- Wrapper for selected Display frontend.
+
+#ifdef CURSES
+import Game.LambdaHack.Frontend.Curses as D
+#elif VTY
+import Game.LambdaHack.Frontend.Vty as D
+#elif STD
+import Game.LambdaHack.Frontend.Std as D
+#else
+import Game.LambdaHack.Frontend.Gtk as D
+#endif
