98d6e1e8b01187854264212ab8c01436ea495867
Mikolaj <mikolaj.konarski@gmail.com>
2013-11-13 15:50:04 +0100

    Add two more screensaver tests

--- LambdaHack/Content/ModeKind.hs
+++ LambdaHack/Content/ModeKind.hs
@@ -14,9 +14,9 @@ cdefs = ContentDef
   , getFreq = mfreq
   , validate = mvalidate
   , content =
-      [campaign, skirmish, pvp, coop, defense, screensaver]
+      [campaign, skirmish, pvp, coop, defense, screensaver, testCoop, testDefense]
   }
-campaign,        skirmish, pvp, coop, defense, screensaver :: ModeKind
+campaign,        skirmish, pvp, coop, defense, screensaver, testCoop, testDefense :: ModeKind
 
 campaign = ModeKind
   { msymbol  = 'r'  -- matches the keypress (with C-)
@@ -66,8 +66,24 @@ screensaver = ModeKind
   , mcaves   = cavesScreensaver
   }
 
+testCoop = ModeKind
+  { msymbol  = 't'
+  , mname    = "testCoop"
+  , mfreq    = [("testCoop", 1)]
+  , mplayers = playersTestCoop
+  , mcaves   = cavesScreensaver
+  }
 
-playersCampaign, playersSkirmish, playersPvP, playersCoop, playersDefense, playersScreensaver :: Players
+testDefense = ModeKind
+  { msymbol  = 'u'
+  , mname    = "testDefense"
+  , mfreq    = [("testDefense", 1)]
+  , mplayers = playersTestDefense
+  , mcaves   = cavesDefense
+  }
+
+
+playersCampaign, playersSkirmish, playersPvP, playersCoop, playersDefense, playersScreensaver, playersTestCoop, playersTestDefense :: Players
 
 playersCampaign = Players
   { playersList = [ playerHero {playerInitial = 1}
@@ -126,6 +142,32 @@ playersScreensaver = Players
                   , playerMonster ]
   , playersEnemy = [("Adventuring Party", "Monster Hive")]
   , playersAlly = [] }
+
+playersTestCoop = Players
+  { playersList = [ playerHero { playerName = "Coral"
+                               , playerAiLeader = True
+                               , playerHuman = False }
+                  , playerHero { playerName = "Amber"
+                               , playerAiLeader = True
+                               , playerHuman = False }
+                  , playerMonster ]
+  , playersEnemy = [ ("Coral", "Monster Hive")
+                   , ("Amber", "Monster Hive") ]
+  , playersAlly = [("Coral", "Amber")] }
+
+playersTestDefense = Players
+  { playersList = [ playerMonster { playerInitial = 1
+                                  , playerEntry = toEnum 1
+                                  , playerUI = True }
+                  , playerAntiHero {playerName = "Green"}
+                  , playerAntiHero {playerName = "Yellow"}
+                  , playerAntiHero {playerName = "Cyan"} ]
+  , playersEnemy = [ ("Green", "Monster Hive")
+                   , ("Yellow", "Monster Hive")
+                   , ("Cyan", "Monster Hive") ]
+  , playersAlly = [ ("Green", "Yellow")
+                  , ("Green", "Cyan")
+                  , ("Yellow", "Cyan") ] }
 
 
 playerHero, playerAntiHero, playerMonster, playerHorror :: Player
--- Makefile
+++ Makefile
@@ -5,7 +5,26 @@ test-frontend:
 	dist/build/LambdaHack/LambdaHack --noMore --maxFps 45 --savePrefix screensaver --gameMode screensaver
 
 test-travis:
-	timeout 1m dist/build/LambdaHack/LambdaHack --noMore --noDelay --noAnim --maxFps 100000 --savePrefix screensaver --gameMode screensaver --frontendStd > stdtest.log ; EXIT=$$? ; (if [ $$EXIT -eq 124 ] ; then echo "test OK" ; else (echo "test failed with $$EXIT" ; exit 1) ; fi)
+	timeout 1m dist/build/LambdaHack/LambdaHack --noMore --noDelay --noAnim --maxFps 100000 --savePrefix screensaver --gameMode screensaver --frontendStd > /dev/null ; EXIT=$$? ; (if [ $$EXIT -eq 124 ] ; then echo "test OK" ; else (echo "test failed with $$EXIT" ; exit 1) ; fi)
+
+testCoop:
+	timeout 10m dist/build/LambdaHack/LambdaHack --noMore --noDelay --noAnim --maxFps 100000 --savePrefix testCoop --gameMode testCoop --frontendStd > /tmp/stdtest.log; EXIT=$$? ; (if [ $$EXIT -eq 124 ] ; then echo "test OK" ; else (echo "test failed with $$EXIT" ; exit 1) ; fi)
+
+testCoop-frontend:
+	dist/build/LambdaHack/LambdaHack --noMore --maxFps 45 --savePrefix testCoop --gameMode testCoop
+
+testCoop-travis:
+	timeout 1m dist/build/LambdaHack/LambdaHack --noMore --noDelay --noAnim --maxFps 100000 --savePrefix testCoop --gameMode testCoop --frontendStd > /dev/null ; EXIT=$$? ; (if [ $$EXIT -eq 124 ] ; then echo "test OK" ; else (echo "test failed with $$EXIT" ; exit 1) ; fi)
+
+testDefense:
+	timeout 10m dist/build/LambdaHack/LambdaHack --noMore --noDelay --noAnim --maxFps 100000 --savePrefix testDefense --gameMode testDefense --frontendStd > /tmp/stdtest.log; EXIT=$$? ; (if [ $$EXIT -eq 124 ] ; then echo "test OK" ; else (echo "test failed with $$EXIT" ; exit 1) ; fi)
+
+testDefense-frontend:
+	dist/build/LambdaHack/LambdaHack --noMore --maxFps 45 --savePrefix testDefense --gameMode testDefense
+
+testDefense-travis:
+	timeout 1m dist/build/LambdaHack/LambdaHack --noMore --noDelay --noAnim --maxFps 100000 --savePrefix testDefense --gameMode testDefense --frontendStd > /dev/null ; EXIT=$$? ; (if [ $$EXIT -eq 124 ] ; then echo "test OK" ; else (echo "test failed with $$EXIT" ; exit 1) ; fi)
+
 
 # The rest of the makefile is unmaintained at the moment.
 
