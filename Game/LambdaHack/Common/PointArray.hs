b537f4de37fc0109e27d221ee69d4272e81bd9ca
Mikolaj <mikolaj.konarski@gmail.com>
2014-01-15 23:55:15 +0100

    Switch PointArray to a single vector
    Simpler and faster, except for many small immutable updates
    which now involve copying the whole vector.

--- Game/LambdaHack/Common/PointArray.hs
+++ Game/LambdaHack/Common/PointArray.hs
@@ -1,15 +1,14 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -- | Arrays, based on Data.Vector.Unboxed, indexed by @Point@.
 module Game.LambdaHack.Common.PointArray
-  ( Array, (!), (//), replicateA, replicateMA, generateMA, sizeA, foldlA
+  ( Array
+  , (!), (//), replicateA, replicateMA, generateMA, sizeA, foldlA, ifoldlA
   ) where
 
+import Control.Arrow ((***))
 import Control.Monad
 import Data.Binary
-import qualified Data.Vector as B (Vector)
 import Data.Vector.Binary ()
-import qualified Data.Vector.Generic as V
-import qualified Data.Vector.Unboxed as U (Vector)
+import qualified Data.Vector.Unboxed as U
 
 import Game.LambdaHack.Common.Point
 import Game.LambdaHack.Common.PointXY
@@ -20,8 +19,12 @@ import Game.LambdaHack.Common.PointXY
 
 -- TODO: perhaps make them an instance of Data.Vector.Generic?
 -- | Arrays indexed by @Point@.
-newtype Array c = Array (B.Vector (U.Vector Word8))
-  deriving (Eq, Binary)
+data Array c = Array
+  { axsize  :: X
+  , aysize  :: Y
+  , avector :: U.Vector Word8
+  }
+  deriving Eq
 
 instance Show (Array c) where
   show a = "PointArray.Array with size " ++ show (sizeA a)
@@ -29,52 +32,69 @@ instance Show (Array c) where
 cnv :: (Enum a, Enum b) => a -> b
 cnv = toEnum . fromEnum
 
+pindex :: X -> Point -> Int
+pindex xsize p = let PointXY x y = fromPoint p
+                 in y * xsize + x
+
+punindex :: X -> Int -> Point
+punindex xsize n = let (y, x) = n `quotRem` xsize
+                   in toPoint $ PointXY x y
+
 -- Note: there's no point specializing this to @PointXY@ arguments,
 -- since the extra few additions in @fromPoint@ may be less expensive than
 -- memory or register allocations needed for the extra @Int@ in @PointXY@.
 -- | Array lookup.
 (!) :: Enum c => Array c -> Point -> c
 {-# INLINE (!) #-}
-(!) (Array a) p = let PointXY x y = fromPoint p
-                  in cnv $ a V.! y V.! x
+(!) Array{..} p = cnv $ avector U.! pindex axsize p
 
 -- TODO: optimize, either by replacing with a different operation
 -- or by thawing all the vectors, updating, freezing.
 -- | Construct an array updated with the association list.
 (//) :: Enum c => Array c -> [(Point, c)] -> Array c
-(//) (Array a) l =
-  let f b (x, e) = b V.// [(x, e)]
-  in Array $ V.accum f a [ (y, (x, cnv e))
-                         | (p, e) <- l, let PointXY x y = fromPoint p ]
+(//) Array{..} l = let v = avector U.// map (pindex axsize *** cnv) l
+                   in Array{avector = v, ..}
 
 -- | Create an array from a replicated element.
 replicateA :: Enum c => X -> Y -> c -> Array c
-replicateA x y e =
-  let line = V.replicate x $ cnv e
-  in Array $ V.replicate y line
+replicateA axsize aysize c =
+  Array{avector = U.replicate (axsize * aysize) $ cnv c, ..}
 
 -- | Create an  array from a replicated monadic action.
 replicateMA :: Enum c => Monad m => X -> Y -> m c -> m (Array c)
-replicateMA x y m =
-  let line = V.replicateM x (liftM cnv m)
-  in liftM Array $ V.replicateM y line
+replicateMA axsize aysize m = do
+  v <- U.replicateM (axsize * aysize) $ liftM cnv m
+  return Array{avector = v, ..}
 
 -- | Create an array from a monadic function.
 generateMA :: Enum c => Monad m => X -> Y -> (Point -> m c) -> m (Array c)
-generateMA x y m =
-  let me y1 x1 = liftM cnv $ m $ toPoint $ PointXY x1 y1
-      mline y1 = V.generateM x (me y1)
-  in liftM Array $ V.generateM y mline
+generateMA axsize aysize fm = do
+  let gm n = liftM cnv $ fm $ punindex axsize n
+  v <- U.generateM (axsize * aysize) gm
+  return Array{avector = v, ..}
 
 -- | Content identifiers array size.
 sizeA :: Array c -> (X, Y)
-sizeA (Array a) =
-  let y = V.length a
-      x = V.length (a V.! 0)
-  in (x, y)
+sizeA Array{..} = (axsize, aysize)
 
 -- | Fold left strictly over an array.
 foldlA :: Enum c => (a -> c -> a) -> a -> Array c -> a
-foldlA f z0 (Array a) = lgo z0 $ concatMap V.toList $ V.toList a
- where lgo z []       = z
-       lgo z (x : xs) = let fzx = f z (cnv x) in fzx `seq` lgo fzx xs
+foldlA f z0 Array{..} =
+  U.foldl' (\a c -> f a (cnv c)) z0 avector
+
+-- | Fold left strictly over an array
+-- (function applied to each element and its index).
+ifoldlA :: Enum c => (a -> Point -> c -> a) -> a -> Array c -> a
+ifoldlA f z0 Array{..} =
+  U.ifoldl' (\a n c -> f a (punindex axsize n) (cnv c)) z0 avector
+
+instance Binary (Array c) where
+  put Array{..} = do
+    put axsize
+    put aysize
+    put avector
+  get = do
+    axsize <- get
+    aysize <- get
+    avector <- get
+    return Array{..}
