1369a4ab40a1da400c2dd10052eed1db74042f18
Mikolaj <mikolaj.konarski@gmail.com>
2014-03-11 16:04:23 +0100

    Prod the frontend to flush frames at the game start

--- Game/LambdaHack/Client/LoopAction.hs
+++ Game/LambdaHack/Client/LoopAction.hs
@@ -5,17 +5,21 @@ module Game.LambdaHack.Client.LoopAction (loopAI, loopUI) where
 
 import Control.Exception.Assert.Sugar
 import Control.Monad
+import qualified Data.EnumMap.Strict as EM
 import qualified Data.Text as T
 
 import Game.LambdaHack.Client.Action
+import Game.LambdaHack.Client.Draw
 import Game.LambdaHack.Client.State
 import Game.LambdaHack.Common.Action
 import Game.LambdaHack.Common.Animation
 import Game.LambdaHack.Common.AtomicCmd
 import Game.LambdaHack.Common.ClientCmd
+import Game.LambdaHack.Common.Faction
 import qualified Game.LambdaHack.Common.Kind as Kind
 import Game.LambdaHack.Common.Msg
 import Game.LambdaHack.Common.State
+import Game.LambdaHack.Content.ModeKind
 import Game.LambdaHack.Content.RuleKind
 
 initCli :: MonadClient m => DebugModeCli -> (State -> m ()) -> m Bool
@@ -73,25 +77,28 @@ loopUI sdebugCli cmdClientUISem = do
   restored <- initCli sdebugCli
               $ \s -> cmdClientUISem $ CmdAtomicUI $ ResumeServerA s
   cmd1 <- readServer
-  case (restored, cmd1) of
+  msg <- case (restored, cmd1) of
     (True, CmdAtomicUI ResumeA{}) -> do
-      let msg = "Welcome back to" <+> title <> "."
       cmdClientUISem cmd1
-      msgAdd msg
+      return $! "Welcome back to" <+> title <> "."
     (True, CmdAtomicUI RestartA{}) -> do
-      let msg = "Starting a new" <+> title <+> "game."  -- ignore old savefile
       cmdClientUISem cmd1
-      msgAdd msg
+      return $! "Starting a new" <+> title <+> "game."  -- ignore old savefile
     (False, CmdAtomicUI ResumeA{}) -> do
       removeServerSave
       error $ T.unpack $
         "Savefile of client" <+> tshow side
         <+> "not usable. Removing server savefile. Please restart now."
     (False, CmdAtomicUI RestartA{}) -> do
-      let msg = "Welcome to" <+> title <> "!"
       cmdClientUISem cmd1
-      msgAdd msg
+      return $! "Welcome to" <+> title <> "!"
     _ -> assert `failure` "unexpected command" `twith` (side, restored, cmd1)
+  fact <- getsState $ (EM.! side) . sfactionD
+  if playerAiLeader $ gplayer fact then
+    -- Prod the frontend to flush frames and start showing then continuously.
+    void $ displayMore ColorFull "The team is under AI control (ESC to stop)."
+  else
+    msgAdd msg
   -- State and client state now valid.
   debugPrint $ "UI client" <+> tshow side <+> "started."
   loop
