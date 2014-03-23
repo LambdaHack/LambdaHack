8a9b55729f7d1f484ad0f944bbb4a990620a50b8
Mikolaj <mikolaj.konarski@gmail.com>
2014-03-24 00:34:43 +0100

    Improve the documentation of Main.hs

--- GameDefinition/Main.hs
+++ GameDefinition/Main.hs
@@ -1,9 +1,9 @@
--- | The main source code file of LambdaHack. Here the knot of engine
--- code pieces and the LambdaHack-specific content defintions is tied,
+-- | The main source code file of LambdaHack, the game. Here the knot of engine
+-- code pieces and the LambdaHack-specific content definitions is tied,
 -- resulting in an executable game.
 module Main ( main ) where
 
-import qualified Client.UI.Content.KeyKind as KeyKind
+import qualified Client.UI.Content.KeyKind as Content.KeyKind
 import qualified Content.ActorKind
 import qualified Content.CaveKind
 import qualified Content.FactionKind
@@ -12,17 +12,18 @@ import qualified Content.ModeKind
 import qualified Content.PlaceKind
 import qualified Content.RuleKind
 import qualified Content.TileKind
-import Game.LambdaHack.Client
+import Game.LambdaHack.Client (exeFrontend)
 import qualified Game.LambdaHack.Common.Kind as Kind
-import Game.LambdaHack.SampleImplementation.SampleImplementationMonadClient
-import Game.LambdaHack.SampleImplementation.SampleImplementationMonadServer
-import Game.LambdaHack.Server
+import Game.LambdaHack.SampleImplementation.SampleImplementationMonadClient (executorCli)
+import Game.LambdaHack.SampleImplementation.SampleImplementationMonadServer (executorSer)
+import Game.LambdaHack.Server (mainSer)
 
--- | Tie the LambdaHack engine clients and server code
--- with the LambdaHack-specific content defintions and run the game.
+-- | Tie the LambdaHack engine client, server and frontend code
+-- with the LambdaHack-specific content definitions, and run the game.
 main :: IO ()
 main =
-  let copsSlow = Kind.COps
+  let -- Server content operations, created from server content definitions.
+      copsServer = Kind.COps
         { coactor   = Kind.createOps Content.ActorKind.cdefs
         , cocave    = Kind.createOps Content.CaveKind.cdefs
         , cofaction = Kind.createOps Content.FactionKind.cdefs
@@ -32,6 +33,9 @@ main =
         , corule    = Kind.createOps Content.RuleKind.cdefs
         , cotile    = Kind.createOps Content.TileKind.cdefs
         }
-      copsClient = KeyKind.standardKeys
-  in mainSer copsSlow executorSer
-     $ exeFrontend executorCli executorCli copsClient
+      -- Client content operations.
+      copsClient = Content.KeyKind.standardKeys
+      -- A single frontend is currently started by the server,
+      -- instead of each client starting it's own.
+      startupFrontend = exeFrontend executorCli executorCli copsClient
+  in mainSer copsServer executorSer startupFrontend
