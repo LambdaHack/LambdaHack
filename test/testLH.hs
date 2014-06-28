1d0b2887a5e09e41c8bb34cf11c06fd9143c518c
Mikolaj <mikolaj.konarski@gmail.com>
2014-06-28 16:54:39 +0200

    Add a simple cabal test

---
+++ test/testLH.hs
@@ -0,0 +1,6 @@
+import TieKnot
+
+main :: IO ()
+main = do
+  tieKnot $ tail $ words "dist/build/LambdaHack/LambdaHack --dbgMsgSer --savePrefix test --newGame --noMore --noDelay --noAnim --maxFps 100000 --frontendNull --benchmark --stopAfter 6 --automateAll --gameMode campaign --difficulty 1 --setDungeonRng 42 --setMainRng 42"
+  -- tieKnot $ tail $ words "dist/build/LambdaHack/LambdaHack --dbgMsgSer --savePrefix test --newGame --noMore --noDelay --noAnim --maxFps 100000 --frontendNull --benchmark --stopAfter 6 --automateAll --gameMode battle --difficulty 1 --setDungeonRng 42 --setMainRng 42"
