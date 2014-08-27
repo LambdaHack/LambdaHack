5318148c9679677453aba578ed7e4b902e52767e
Mikolaj <mikolaj.konarski@gmail.com>
2014-08-28 01:49:48 +0200

    Modify vty frontend to work with vty 5.*

--- Game/LambdaHack/Client/UI/Frontend/Vty.hs
+++ Game/LambdaHack/Client/UI/Frontend/Vty.hs
@@ -11,6 +11,7 @@ module Game.LambdaHack.Client.UI.Frontend.Vty
 import Control.Concurrent
 import Control.Concurrent.Async
 import qualified Control.Exception as Ex hiding (handle)
+import Data.Default
 import Graphics.Vty
 import qualified Graphics.Vty as Vty
 
@@ -35,7 +36,7 @@ frontendName = "vty"
 -- | Starts the main program loop using the frontend input and output.
 startup :: DebugModeCli -> (FrontendSession -> IO ()) -> IO ()
 startup sdebugCli k = do
-  svty <- mkVty
+  svty <- mkVty def
   -- TODO: implement sescMVar, when we switch to a new vty that has
   -- a separate key listener thread or something. Avoid polling.
   a <- async $ k FrontendSession{sescMVar = Nothing, ..}
@@ -50,24 +51,24 @@ fdisplay :: FrontendSession    -- ^ frontend session data
 fdisplay _ _ Nothing = return ()
 fdisplay FrontendSession{svty} _ (Just rawSF) =
   let SingleFrame{sfLevel} = overlayOverlay rawSF
-      img = (foldr (<->) empty_image
-             . map (foldr (<|>) empty_image
+      img = (foldr (<->) emptyImage
+             . map (foldr (<|>) emptyImage
                       . map (\ Color.AttrChar{..} ->
                                 char (setAttr acAttr) acChar)))
             $ map decodeLine sfLevel
-      pic = pic_for_image img
+      pic = picForImage img
   in update svty pic
 
 -- | Input key via the frontend.
-nextEvent :: FrontendSession -> IO K.KM
-nextEvent sess@FrontendSession{svty} = do
-  e <- next_event svty
+nextKeyEvent :: FrontendSession -> IO K.KM
+nextKeyEvent sess@FrontendSession{svty} = do
+  e <- nextEvent svty
   case e of
     EvKey n mods -> do
       let key = keyTranslate n
           modifier = modifierTranslate mods
       return $! K.KM {key, modifier}
-    _ -> nextEvent sess
+    _ -> nextKeyEvent sess
 
 fsyncFrames :: FrontendSession -> IO ()
 fsyncFrames _ = return ()
@@ -76,7 +77,7 @@ fsyncFrames _ = return ()
 fpromptGetKey :: FrontendSession -> SingleFrame -> IO K.KM
 fpromptGetKey sess frame = do
   fdisplay sess True $ Just frame
-  nextEvent sess
+  nextKeyEvent sess
 
 -- TODO: Ctrl-Home and Ctrl-End are the same as Home and End on some terminals
 -- so we should probably go back to using 0-9 (and Shift) for movement
@@ -86,8 +87,8 @@ keyTranslate n =
   case n of
     KEsc          -> K.Esc
     KEnter        -> K.Return
-    (KASCII ' ')  -> K.Space
-    (KASCII '\t') -> K.Tab
+    (KChar ' ')   -> K.Space
+    (KChar '\t')  -> K.Tab
     KBackTab      -> K.BackTab
     KBS           -> K.BackSpace
     KUp           -> K.Up
@@ -99,8 +100,8 @@ keyTranslate n =
     KPageUp       -> K.PgUp
     KPageDown     -> K.PgDn
     KBegin        -> K.Begin
-    KNP5          -> K.Begin
-    (KASCII c)    -> K.Char c
+    KCenter       -> K.Begin
+    (KChar c)     -> K.Char c
     _             -> K.Unknown (tshow n)
 
 -- | Translates modifiers to our own encoding.
@@ -113,7 +114,7 @@ modifierTranslate mods =
 -- settings this is needed or not and the characters really get bold or not.
 -- HSCurses does this by default, but in Vty you have to request the hack.
 hack :: Color.Color -> Attr -> Attr
-hack c a = if Color.isBright c then with_style a bold else a
+hack c a = if Color.isBright c then withStyle a bold else a
 
 setAttr :: Color.Attr -> Attr
 setAttr Color.Attr{fg, bg} =
@@ -122,8 +123,8 @@ setAttr Color.Attr{fg, bg} =
 --  then def_attr
 --  else
   hack fg $ hack bg $
-    def_attr { attr_fore_color = SetTo (aToc fg)
-             , attr_back_color = SetTo (aToc bg) }
+    defAttr { attrForeColor = SetTo (aToc fg)
+            , attrBackColor = SetTo (aToc bg) }
 
 aToc :: Color.Color -> Color
 aToc Color.Black     = black
@@ -134,11 +135,11 @@ aToc Color.Blue      = blue
 aToc Color.Magenta   = magenta
 aToc Color.Cyan      = cyan
 aToc Color.White     = white
-aToc Color.BrBlack   = bright_black
-aToc Color.BrRed     = bright_red
-aToc Color.BrGreen   = bright_green
-aToc Color.BrYellow  = bright_yellow
-aToc Color.BrBlue    = bright_blue
-aToc Color.BrMagenta = bright_magenta
-aToc Color.BrCyan    = bright_cyan
-aToc Color.BrWhite   = bright_white
+aToc Color.BrBlack   = brightBlack
+aToc Color.BrRed     = brightRed
+aToc Color.BrGreen   = brightGreen
+aToc Color.BrYellow  = brightYellow
+aToc Color.BrBlue    = brightBlue
+aToc Color.BrMagenta = brightMagenta
+aToc Color.BrCyan    = brightCyan
+aToc Color.BrWhite   = brightWhite
