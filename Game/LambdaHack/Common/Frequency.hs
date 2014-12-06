e0c401d449e8347e9c3fda0edb3828bb14b99361
Mikolaj <mikolaj.konarski@gmail.com>
2014-12-06 01:13:04 +0100

    Reorder the fields of Frequency for a more useful Ord derivation

--- Game/LambdaHack/Common/Frequency.hs
+++ Game/LambdaHack/Common/Frequency.hs
@@ -35,39 +35,39 @@ import Game.LambdaHack.Common.Msg
 -- the expected equalities, unless they do some kind of normalization
 -- (see 'Dice').
 data Frequency a = Frequency
-  { nameFrequency :: Text         -- ^ short description for debug, etc.;
+  { runFrequency  :: ![(Int, a)]  -- ^ give acces to raw frequency values
+  , nameFrequency :: Text         -- ^ short description for debug, etc.;
                                   --   keep it lazy, because it's rarely used
-  , runFrequency  :: ![(Int, a)]  -- ^ give acces to raw frequency values
   }
   deriving (Show, Read, Eq, Ord, Foldable, Traversable, Generic)
 
 instance Monad Frequency where
   {-# INLINE return #-}
-  return x = Frequency "return" [(1, x)]
-  Frequency name xs >>= f =
-    Frequency ("bind (" <> name <> ")")
-              [ (p * q, y) | (p, x) <- xs
+  return x = Frequency [(1, x)] "return"
+  Frequency xs name >>= f =
+    Frequency [ (p * q, y) | (p, x) <- xs
                            , (q, y) <- runFrequency (f x) ]
+              ("bind (" <> name <> ")")
 
 instance Functor Frequency where
-  fmap f (Frequency name xs) = Frequency name (map (second f) xs)
+  fmap f (Frequency xs name) = Frequency (map (second f) xs) name
 
 instance Applicative Frequency where
   pure  = return
-  Frequency fname fs <*> Frequency yname ys =
-    Frequency ("(" <> fname <> ") <*> (" <> yname <> ")")
-              [ (p * q, f y) | (p, f) <- fs
+  Frequency fs fname <*> Frequency ys yname =
+    Frequency [ (p * q, f y) | (p, f) <- fs
                              , (q, y) <- ys ]
+              ("(" <> fname <> ") <*> (" <> yname <> ")")
 
 instance MonadPlus Frequency where
-  mplus (Frequency xname xs) (Frequency yname ys) =
+  mplus (Frequency xs xname) (Frequency ys yname) =
     let name = case (xs, ys) of
           ([], []) -> "[]"
           ([], _ ) -> yname
           (_,  []) -> xname
           _ -> "(" <> xname <> ") ++ (" <> yname <> ")"
-    in Frequency name (xs ++ ys)
-  mzero = Frequency "[]" []
+    in Frequency (xs ++ ys) name
+  mzero = Frequency [] "[]"
 
 instance Alternative Frequency where
   (<|>) = mplus
@@ -79,19 +79,19 @@ instance Binary a => Binary (Frequency a)
 
 -- | Uniform discrete frequency distribution.
 uniformFreq :: Text -> [a] -> Frequency a
-uniformFreq name = Frequency name . map (\x -> (1, x))
+uniformFreq name l = Frequency (map (\x -> (1, x)) l) name
 
 -- | Takes a name and a list of frequencies and items
 -- into the frequency distribution.
 toFreq :: Text -> [(Int, a)] -> Frequency a
-toFreq = Frequency
+toFreq = flip Frequency
 
 -- | Scale frequecy distribution, multiplying it
 -- by a positive integer constant.
 scaleFreq :: Show a => Int -> Frequency a -> Frequency a
-scaleFreq n (Frequency name xs) =
+scaleFreq n (Frequency xs name) =
   assert (n > 0 `blame` "non-positive frequency scale" `twith` (name, n, xs)) $
-  Frequency name (map (first (* n)) xs)
+  Frequency (map (first (* n)) xs) name
 
 -- | Change the description of the frequency.
 renameFreq :: Text -> Frequency a -> Frequency a
@@ -99,27 +99,27 @@ renameFreq newName fr = fr {nameFrequency = newName}
 
 -- | Set frequency of an element.
 setFreq :: Eq a => Frequency a -> a -> Int -> Frequency a
-setFreq (Frequency name xs) x n =
+setFreq (Frequency xs name) x n =
   let f (_, y) | y == x = (n, x)
       f my = my
-  in Frequency name $ map f xs
+  in Frequency (map f xs) name
 
 -- | Test if the frequency distribution is empty.
 nullFreq :: Frequency a -> Bool
-nullFreq (Frequency _ fs) = all (<= 0) $ map fst fs
+nullFreq (Frequency fs _) = all (<= 0) $ map fst fs
 
 maxFreq :: (Show a, Ord a) => Frequency a -> a
-maxFreq fr@(Frequency _ xs) = case filter ((> 0 ) . fst) xs of
+maxFreq fr@(Frequency xs _) = case filter ((> 0 ) . fst) xs of
   [] -> assert `failure` fr
   ys -> maximum $ map snd ys
 
 minFreq :: (Show a, Ord a) => Frequency a -> a
-minFreq fr@(Frequency _ xs) = case filter ((> 0 ) . fst) xs of
+minFreq fr@(Frequency xs _) = case filter ((> 0 ) . fst) xs of
   [] -> assert `failure` fr
   ys -> minimum $ map snd ys
 
 meanFreq :: (Show a, Integral a) => Frequency a -> Rational
-meanFreq fr@(Frequency _ xs) = case filter ((> 0 ) . fst) xs of
+meanFreq fr@(Frequency xs _) = case filter ((> 0 ) . fst) xs of
   [] -> assert `failure` fr
   ys -> let sumP = sum $ map fst ys
             sumX = sum [ fromIntegral p * x | (p, x) <- ys ]
