460e23ad19937f01272b4c16bdfb3a1e15356187
Mikolaj <mikolaj.konarski@gmail.com>
2015-01-06 14:35:53 +0100

    Re-add pushing to shields now that they are used to attack sometimes

--- GameDefinition/Content/ItemKind.hs
+++ GameDefinition/Content/ItemKind.hs
@@ -740,7 +740,7 @@ buckler = ItemKind
   , iaspects = [ AddArmorMelee 40
                , AddHurtMelee (-30)
                , Timeout $ d 3 + 3 - dl 3 |*| 2 ]
-  , ieffects = []  -- [Recharging (PushActor (ThrowMod 200 50))]
+  , ieffects = [Hurt (1 * d 1), Recharging (PushActor (ThrowMod 200 50))]
   , ifeature = [ toVelocity 30  -- unwieldy to throw and blunt
                , Durable, EqpSlot EqpSlotAddArmorMelee "", Identified ]
   , idesc    = "Heavy and unwieldy. Absorbs a percentage of melee damage, both dealt and sustained. Too small to intercept projectiles with."
@@ -754,7 +754,7 @@ shield = buckler
   , iaspects = [ AddArmorMelee 80
                , AddHurtMelee (-70)
                , Timeout $ d 3 + 6 - dl 3 |*| 2 ]
-  , ieffects = []  -- [Recharging (PushActor (ThrowMod 400 50))]
+  , ieffects = [Hurt (1 * d 1), Recharging (PushActor (ThrowMod 400 50))]
   , ifeature = [ toVelocity 20  -- unwieldy to throw and blunt
                , Durable, EqpSlot EqpSlotAddArmorMelee "", Identified ]
   , idesc    = "Large and unwieldy. Absorbs a percentage of melee damage, both dealt and sustained. Too heavy to intercept projectiles with."
