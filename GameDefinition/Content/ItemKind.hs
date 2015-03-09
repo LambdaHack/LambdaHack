4b2ca0fac5b726896e4002bed72bac75eda4f630
Mikolaj <mikolaj.konarski@gmail.com>
2015-03-09 18:49:11 +0100

    Make gems less common and elixirs more

--- GameDefinition/Content/ItemKind.hs
+++ GameDefinition/Content/ItemKind.hs
@@ -953,18 +953,18 @@ gem = ItemKind
   , ikit     = []
   }
 gem1 = gem
-  { irarity  = [(2, 0), (10, 10)]
+  { irarity  = [(2, 0), (10, 12)]
   }
 gem2 = gem
-  { irarity  = [(4, 0), (10, 15)]
+  { irarity  = [(4, 0), (10, 14)]
   }
 gem3 = gem
-  { irarity  = [(6, 0), (10, 20)]
+  { irarity  = [(6, 0), (10, 16)]
   }
 gem4 = gem
   { iname    = "elixir"
   , iflavour = zipPlain [BrYellow]
-  , irarity  = [(1, 15), (10, 15)]
+  , irarity  = [(1, 25), (10, 25)]
   , iaspects = []
   , ieffects = [NoEffect "of youth", OverfillCalm 5, OverfillHP 15]
   , ifeature = [Identified, Precious]  -- TODO: only for humans
