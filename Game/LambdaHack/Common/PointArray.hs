174d44e62ed468a9f9404c39bcf49f2923378680
Mikolaj <mikolaj.konarski@gmail.com>
2016-09-16 02:11:07 +0200

    Generalize PointArray to any Unbox type

--- Game/LambdaHack/Common/PointArray.hs
+++ Game/LambdaHack/Common/PointArray.hs
@@ -1,6 +1,6 @@
 -- | Arrays, based on Data.Vector.Unboxed, indexed by @Point@.
 module Game.LambdaHack.Common.PointArray
-  ( Array(..), pindex, punindex
+  ( GArray(..), Array, pindex, punindex
   , (!), accessI, (//)
   , replicateA, replicateMA, generateA, generateMA, unfoldrNA, sizeA
   , foldrA, foldlA', ifoldrA, ifoldrA', ifoldlA', mapA, imapA, mapWithKeyMA
@@ -34,15 +34,18 @@ import Game.LambdaHack.Common.Point
 
 -- TODO: perhaps make them an instance of Data.Vector.Generic?
 -- | Arrays indexed by @Point@.
-data Array c = Array
+data GArray w c = Array
   { axsize  :: !X
   , aysize  :: !Y
-  , avector :: !(U.Vector Word8)
+  , avector :: !(U.Vector w)
   }
   deriving Eq
 
-instance Show (Array c) where
+instance Show (GArray w c) where
   show a = "PointArray.Array with size " ++ show (sizeA a)
+
+-- | Arrays of, effectively, @Word8@, indexed by @Point@.
+type Array c = GArray Word8 c
 
 cnv :: (Enum a, Enum b) => a -> b
 {-# INLINE cnv #-}
@@ -63,35 +66,35 @@ punindex xsize n = let (y, x) = n `quotRem` xsize
 -- since the extra few additions in @fromPoint@ may be less expensive than
 -- memory or register allocations needed for the extra @Int@ in @Point@.
 -- | Array lookup.
-(!) :: Enum c => Array c -> Point -> c
+(!) :: (U.Unbox w, Enum w, Enum c) => GArray w c -> Point -> c
 {-# INLINE (!) #-}
 (!) Array{..} p = cnv $ avector U.! pindex axsize p
 
-accessI :: Enum c => Array c -> Int -> c
+accessI :: (U.Unbox w, Enum w, Enum c) => GArray w c -> Int -> c
 {-# INLINE accessI #-}
 accessI Array{..} p = cnv $ avector `U.unsafeIndex` p
 
 -- | Construct an array updated with the association list.
-(//) :: Enum c => Array c -> [(Point, c)] -> Array c
+(//) :: (U.Unbox w, Enum w, Enum c) => GArray w c -> [(Point, c)] -> GArray w c
 {-# INLINE (//) #-}
 (//) Array{..} l = let v = avector U.// map (pindex axsize *** cnv) l
                    in Array{avector = v, ..}
 
-unsafeUpdateA :: Enum c => Array c -> [(Point, c)] -> ()
+unsafeUpdateA :: (U.Unbox w, Enum w, Enum c) => GArray w c -> [(Point, c)] -> ()
 {-# INLINE unsafeUpdateA #-}
 unsafeUpdateA Array{..} l = runST $ do
   vThawed <- U.unsafeThaw avector
   mapM_ (\(p, c) -> VM.write vThawed (pindex axsize p) (cnv c)) l
   void $ U.unsafeFreeze vThawed
 
-unsafeWriteA :: Enum c => Array c -> Point -> c -> ()
+unsafeWriteA :: (U.Unbox w, Enum w, Enum c) => GArray w c -> Point -> c -> ()
 {-# INLINE unsafeWriteA #-}
 unsafeWriteA Array{..} p c = runST $ do
   vThawed <- U.unsafeThaw avector
   VM.write vThawed (pindex axsize p) (cnv c)
   void $ U.unsafeFreeze vThawed
 
-unsafeWriteManyA :: Enum c => Array c -> [Point] -> c -> ()
+unsafeWriteManyA :: (U.Unbox w, Enum w, Enum c) => GArray w c -> [Point] -> c -> ()
 {-# INLINE unsafeWriteManyA #-}
 unsafeWriteManyA Array{..} l c = runST $ do
   vThawed <- U.unsafeThaw avector
@@ -100,34 +103,34 @@ unsafeWriteManyA Array{..} l c = runST $ do
   void $ U.unsafeFreeze vThawed
 
 -- | Create an array from a replicated element.
-replicateA :: Enum c => X -> Y -> c -> Array c
+replicateA :: (U.Unbox w, Enum w, Enum c) => X -> Y -> c -> GArray w c
 {-# INLINE replicateA #-}
 replicateA axsize aysize c =
   Array{avector = U.replicate (axsize * aysize) $ cnv c, ..}
 
 -- | Create an array from a replicated monadic action.
-replicateMA :: Enum c => Monad m => X -> Y -> m c -> m (Array c)
+replicateMA :: (U.Unbox w, Enum w, Enum c, Monad m) => X -> Y -> m c -> m (GArray w c)
 {-# INLINE replicateMA #-}
 replicateMA axsize aysize m = do
   v <- U.replicateM (axsize * aysize) $ liftM cnv m
   return $! Array{avector = v, ..}
 
 -- | Create an array from a function.
-generateA :: Enum c => X -> Y -> (Point -> c) -> Array c
+generateA :: (U.Unbox w, Enum w, Enum c) => X -> Y -> (Point -> c) -> GArray w c
 {-# INLINE generateA #-}
 generateA axsize aysize f =
   let g n = cnv $ f $ punindex axsize n
   in Array{avector = U.generate (axsize * aysize) g, ..}
 
 -- | Create an array from a monadic function.
-generateMA :: Enum c => Monad m => X -> Y -> (Point -> m c) -> m (Array c)
+generateMA :: (U.Unbox w, Enum w, Enum c, Monad m) => X -> Y -> (Point -> m c) -> m (GArray w c)
 {-# INLINE generateMA #-}
 generateMA axsize aysize fm = do
   let gm n = liftM cnv $ fm $ punindex axsize n
   v <- U.generateM (axsize * aysize) gm
   return $! Array{avector = v, ..}
 
-unfoldrNA :: Enum c => X -> Y -> (b -> (c, b)) -> b -> Array c
+unfoldrNA :: (U.Unbox w, Enum w, Enum c) => X -> Y -> (b -> (c, b)) -> b -> GArray w c
 {-# INLINE unfoldrNA #-}
 unfoldrNA axsize aysize fm b =
   let gm = Just . first cnv . fm
@@ -135,32 +138,32 @@ unfoldrNA axsize aysize fm b =
   in Array {avector = v, ..}
 
 -- | Content identifiers array size.
-sizeA :: Array c -> (X, Y)
+sizeA :: GArray w c -> (X, Y)
 {-# INLINE sizeA #-}
 sizeA Array{..} = (axsize, aysize)
 
 -- | Fold right over an array.
-foldrA :: Enum c => (c -> a -> a) -> a -> Array c -> a
+foldrA :: (U.Unbox w, Enum w, Enum c) => (c -> a -> a) -> a -> GArray w c -> a
 {-# INLINE foldrA #-}
 foldrA f z0 Array{..} =
   U.foldr (\c a-> f (cnv c) a) z0 avector
 
 -- | Fold left strictly over an array.
-foldlA' :: Enum c => (a -> c -> a) -> a -> Array c -> a
+foldlA' :: (U.Unbox w, Enum w, Enum c) => (a -> c -> a) -> a -> GArray w c -> a
 {-# INLINE foldlA' #-}
 foldlA' f z0 Array{..} =
   U.foldl' (\a c -> f a (cnv c)) z0 avector
 
 -- | Fold left strictly over an array
 -- (function applied to each element and its index).
-ifoldlA' :: Enum c => (a -> Point -> c -> a) -> a -> Array c -> a
+ifoldlA' :: (U.Unbox w, Enum w, Enum c) => (a -> Point -> c -> a) -> a -> GArray w c -> a
 {-# INLINE ifoldlA' #-}
 ifoldlA' f z0 Array{..} =
   U.ifoldl' (\a n c -> f a (punindex axsize n) (cnv c)) z0 avector
 
 -- | Fold right over an array
 -- (function applied to each element and its index).
-ifoldrA :: Enum c => (Point -> c -> a -> a) -> a -> Array c -> a
+ifoldrA :: (U.Unbox w, Enum w, Enum c) => (Point -> c -> a -> a) -> a -> GArray w c -> a
 {-# INLINE ifoldrA #-}
 ifoldrA f z0 Array{..} =
   U.ifoldr (\n c a -> f (punindex axsize n) (cnv c) a) z0 avector
@@ -173,19 +176,19 @@ ifoldrA' f z0 Array{..} =
   U.ifoldr' (\n c a -> f (punindex axsize n) (cnv c) a) z0 avector
 
 -- | Map over an array.
-mapA :: (Enum c, Enum d) => (c -> d) -> Array c -> Array d
+mapA :: (U.Unbox w, Enum w, Enum c, Enum d) => (c -> d) -> GArray w c -> GArray w d
 {-# INLINE mapA #-}
 mapA f Array{..} = Array{avector = U.map (cnv . f . cnv) avector, ..}
 
 -- | Map over an array (function applied to each element and its index).
-imapA :: (Enum c, Enum d) => (Point -> c -> d) -> Array c -> Array d
+imapA :: (U.Unbox w, Enum w, Enum c, Enum d) => (Point -> c -> d) -> GArray w c -> GArray w d
 {-# INLINE imapA #-}
 imapA f Array{..} =
   let v = U.imap (\n c -> cnv $ f (punindex axsize n) (cnv c)) avector
   in Array{avector = v, ..}
 
 -- | Set all elements to the given value, in place.
-unsafeSetA :: Enum c => c -> Array c -> Array c
+unsafeSetA :: (U.Unbox w, Enum w, Enum c) => c -> GArray w c -> GArray w c
 {-# INLINE unsafeSetA #-}
 unsafeSetA c Array{..} = runST $ do
   vThawed <- U.unsafeThaw avector
@@ -194,28 +197,28 @@ unsafeSetA c Array{..} = runST $ do
   return $! Array{avector = vFrozen, ..}
 
 -- | Set all elements to the given value, in place, if possible.
-safeSetA :: Enum c => c -> Array c -> Array c
+safeSetA :: (U.Unbox w, Enum w, Enum c) => c -> GArray w c -> GArray w c
 {-# INLINE safeSetA #-}
 safeSetA c Array{..} =
   Array{avector = U.modify (\v -> VM.set v (cnv c)) avector, ..}
 
 -- | Map monadically over an array (function applied to each element
 -- and its index) and ignore the results.
-mapWithKeyMA :: Enum c => Monad m
-             => (Point -> c -> m ()) -> Array c -> m ()
+mapWithKeyMA :: (U.Unbox w, Enum w, Enum c, Monad m)
+             => (Point -> c -> m ()) -> GArray w c -> m ()
 {-# INLINE mapWithKeyMA #-}
 mapWithKeyMA f Array{..} =
   U.imapM_ (\n c -> f (punindex axsize n) (cnv c)) avector
 
 -- | Yield the point coordinates of a minimum element of the array.
 -- The array may not be empty.
-minIndexA :: Array c -> Point
+minIndexA :: (U.Unbox w, Ord w) => GArray w c -> Point
 {-# INLINE minIndexA #-}
 minIndexA Array{..} = punindex axsize $ U.minIndex avector
 
 -- | Yield the point coordinates of the last minimum element of the array.
 -- The array may not be empty.
-minLastIndexA :: Array c -> Point
+minLastIndexA :: (U.Unbox w, Ord w) => GArray w c -> Point
 {-# INLINE minLastIndexA #-}
 minLastIndexA Array{..} =
   punindex axsize
@@ -226,7 +229,7 @@ minLastIndexA Array{..} =
 
 -- | Yield the point coordinates of all the minimum elements of the array.
 -- The array may not be empty.
-minIndexesA :: Array c -> [Point]
+minIndexesA :: (U.Unbox w, Enum w, Ord w) => GArray w c -> [Point]
 {-# INLINE minIndexesA #-}
 minIndexesA Array{..} =
   map (punindex axsize)
@@ -238,13 +241,13 @@ minIndexesA Array{..} =
 
 -- | Yield the point coordinates of the first maximum element of the array.
 -- The array may not be empty.
-maxIndexA :: Array c -> Point
+maxIndexA :: (U.Unbox w, Ord w) => GArray w c -> Point
 {-# INLINE maxIndexA #-}
 maxIndexA Array{..} = punindex axsize $ U.maxIndex avector
 
 -- | Yield the point coordinates of the last maximum element of the array.
 -- The array may not be empty.
-maxLastIndexA :: Array c -> Point
+maxLastIndexA :: (U.Unbox w, Ord w) => GArray w c -> Point
 {-# INLINE maxLastIndexA #-}
 maxLastIndexA Array{..} =
   punindex axsize
@@ -254,11 +257,11 @@ maxLastIndexA Array{..} =
   imax (i, x) (j, y) = i `seq` j `seq` if x <= y then (j, y) else (i, x)
 
 -- | Force the array not to retain any extra memory.
-forceA :: Array c -> Array c
+forceA :: U.Unbox w => GArray w c -> GArray w c
 {-# INLINE forceA #-}
 forceA Array{..} = Array{avector = U.force avector, ..}
 
-instance Binary (Array c) where
+instance (U.Unbox w, Binary w) => Binary (GArray w c) where
   put Array{..} = do
     put axsize
     put aysize
