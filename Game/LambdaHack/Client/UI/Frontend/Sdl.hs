dee450549bd9ed021f10c9f59edad9bff3ec70c7
Mikolaj <mikolaj.konarski@gmail.com>
2017-02-14 02:19:51 +0100

    Revert "Prevent overwrite of highlights"
    This reverts commit 5fef56b41c36f7a1dd6955054b37717f2ce12ceb.

--- Game/LambdaHack/Client/UI/Frontend/Sdl.hs
+++ Game/LambdaHack/Client/UI/Frontend/Sdl.hs
@@ -122,8 +122,7 @@ display :: DebugModeCli
 display DebugModeCli{..} FrontendSession{..} SingleFrame{singleFrame} = do
   SDL.rendererDrawColor srenderer SDL.$= SDL.V4 0 0 0 0
   SDL.clear srenderer
-  let xsize = fst normalLevelBound + 1
-      ysize = snd normalLevelBound + 4
+  let lxsize = fst normalLevelBound + 1
       fontSize = fromMaybe 16 sfontSize
       boxSize = fontSize
       vp x y = Vect.P $ Vect.V2 (toEnum x) (toEnum y)
@@ -144,7 +143,7 @@ display DebugModeCli{..} FrontendSession{..} SingleFrame{singleFrame} = do
   let setChar :: Int -> Word32 -> IO ()
       setChar i w = do
         atlas <- readIORef satlas
-        let (y, x) = (xsize * ysize - 1 - i) `divMod` xsize
+        let (y, x) = i `divMod` lxsize
             acRaw = Color.AttrCharW32 w
             Color.AttrChar{acAttr=Color.Attr{bg=bgRaw, fg}, acChar} =
               Color.attrCharFromW32 acRaw
@@ -178,8 +177,7 @@ display DebugModeCli{..} FrontendSession{..} SingleFrame{singleFrame} = do
                                          (SDL.textureHeight ti))
         SDL.copy srenderer textTexture Nothing (Just loc)
         maybe (return ()) (drawHighlight x y) mlineColor
-  -- @reverse@ to prevent overwrite of highlights
-  U.imapM_ setChar (U.reverse $ PointArray.avector singleFrame)
+  U.imapM_ setChar (PointArray.avector singleFrame)
   SDL.present srenderer
 
 -- | Translates modifiers to our own encoding.
