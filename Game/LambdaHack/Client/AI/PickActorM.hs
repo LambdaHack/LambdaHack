ceb631b8ebca1a0a6a777bea69f9259eb4b043f7
Mikolaj <mikolaj.konarski@gmail.com>
2017-05-07 23:24:13 +0200

    Use a more refined notion of blocking when picking leader

--- Game/LambdaHack/Client/AI/PickActorM.hs
+++ Game/LambdaHack/Client/AI/PickActorM.hs
@@ -182,16 +182,25 @@ pickActorToMove maidToAvoid = do
           (oursTEnemyAll, oursOther) = partition targetTEnemy oursNotRanged
           -- These are not necessarily stuck (perhaps can go around),
           -- but their current path is blocked by friends.
-          notSwapReady b TgtAndPath{tapPath=AndPath{pathList=q : _}} =
-            q /= bpos b
+          notSwapReady abt@((_, b), _)
+                       (ab2, Just t2@TgtAndPath{tapPath=
+                                       AndPath{pathList=q : _}}) =
+            let source = bpos b
+                retry = False  -- avoid forced displace, unless all need it
+                enemyTgtOrenemyPos = targetTEnemy abt
+                enemyTgt2OrenemyPos2 = targetTEnemy (ab2, t2)
+            -- Copied from 'displaceTowards':
+            in not (q == source  -- friend wants to swap
+                    || retry  -- desperate
+                    || enemyTgtOrenemyPos && not enemyTgt2OrenemyPos2)
           notSwapReady _ _ = True
-          targetBlocked ((aid, body), TgtAndPath{tapPath}) = case tapPath of
+          targetBlocked abt@((aid, body), TgtAndPath{tapPath}) = case tapPath of
             AndPath{pathList= q : _} ->
                waitedLastTurn body  -- 1 free sidestep
-               && any (\((aid2, body2), mtgt2) ->
+               && any (\abt2@((aid2, body2), _) ->
                          aid2 /= aid  -- in case pushed on goal
                          && bpos body2 == q
-                         && maybe True (notSwapReady body) mtgt2)
+                         && notSwapReady abt abt2)
                       oursTgtRaw
             _ -> False
           (oursTEnemyBlocked, oursTEnemy) =
