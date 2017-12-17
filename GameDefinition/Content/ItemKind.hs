0b9e3ca73277a2b0fa00b520ab6de6d5c11c8796
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2017-12-17 16:49:51 +0100

    Fix whitespace, which is meaningul in Haskell, for the new items
    Now it compiles.

--- GameDefinition/Content/ItemKind.hs
+++ GameDefinition/Content/ItemKind.hs
@@ -35,9 +35,9 @@ otherItemContent = embeds ++ actors ++ organs ++ blasts ++ temporaries
 
 items :: [ItemKind]
 items =
-  [sandstoneRock, dart, spike, slingStone, slingBullet, paralizingProj, harpoon, net, light1, light2, light3, blanket, flask1, flask2, flask3, flask4, flask5, flask6, flask7, flask8, flask9, flask10, flask11, flask12, flask13, flask14, flask15, flask16, flask17, flask18, flask19, flask20, potion1, potion2, potion3, potion4, potion5, potion6, potion7, potion8, potion9, potion10, scroll1, scroll2, scroll3, scroll4, scroll5, scroll6, scroll7, scroll8, scroll9, scroll10, scroll11, scroll12, scroll13, jumpingPole, sharpeningTool, seeingItem, motionScanner, gorget, necklace1, necklace2, necklace3, necklace4, necklace5, necklace6, necklace7, necklace8, necklace9, imageItensifier, sightSharpening, ring1, ring2, ring3, ring4, ring5, ring6, ring7, ring8, armorLeather, armorMail, gloveFencing, gloveGauntlet, gloveJousting, buckler, shield, dagger, daggerDropBestWeapon, hammer, hammerParalyze, hammerSpark, sword, swordImpress, swordNullify, halberd, halberdPushActor, wand1, wand2, gem1, gem2, gem3, gem4, gem5, currenc, explosive, firecrackery]
+  [sandstoneRock, dart, spike, slingStone, slingBullet, paralizingProj, harpoon, net, light1, light2, light3, blanket, flask1, flask2, flask3, flask4, flask5, flask6, flask7, flask8, flask9, flask10, flask11, flask12, flask13, flask14, flask15, flask16, flask17, flask18, flask19, flask20, potion1, potion2, potion3, potion4, potion5, potion6, potion7, potion8, potion9, potion10, scroll1, scroll2, scroll3, scroll4, scroll5, scroll6, scroll7, scroll8, scroll9, scroll10, scroll11, scroll12, scroll13, jumpingPole, sharpeningTool, seeingItem, motionScanner, gorget, necklace1, necklace2, necklace3, necklace4, necklace5, necklace6, necklace7, necklace8, necklace9, imageItensifier, sightSharpening, ring1, ring2, ring3, ring4, ring5, ring6, ring7, ring8, armorLeather, armorMail, gloveFencing, gloveGauntlet, gloveJousting, buckler, shield, dagger, daggerDropBestWeapon, hammer, hammerParalyze, hammerSpark, sword, swordImpress, swordNullify, halberd, halberdPushActor, wand1, wand2, gem1, gem2, gem3, gem4, gem5, currency, explosive, firecracker]
 
-sandstoneRock,    dart, spike, slingStone, slingBullet, paralizingProj, harpoon, net, light1, light2, light3, blanket, flask1, flask2, flask3, flask4, flask5, flask6, flask7, flask8, flask9, flask10, flask11, flask12, flask13, flask14, flask15, flask16, flask17, flask18, flask19, flask20, potion1, potion2, potion3, potion4, potion5, potion6, potion7, potion8, potion9, potion10, scroll1, scroll2, scroll3, scroll4, scroll5, scroll6, scroll7, scroll8, scroll9, scroll10, scroll11, scroll12, scroll13, jumpingPole, sharpeningTool, seeingItem, motionScanner, gorget, necklace1, necklace2, necklace3, necklace4, necklace5, necklace6, necklace7, necklace8, necklace9, imageItensifier, sightSharpening, ring1, ring2, ring3, ring4, ring5, ring6, ring7, ring8, armorLeather, armorMail, gloveFencing, gloveGauntlet, gloveJousting, buckler, shield, dagger, daggerDropBestWeapon, hammer, hammerParalyze, hammerSpark, sword, swordImpress, swordNullify, halberd, halberdPushActor, wand1, wand2, gem1, gem2, gem3, gem4, gem5, currency :: ItemKind
+sandstoneRock,    dart, spike, slingStone, slingBullet, paralizingProj, harpoon, net, light1, light2, light3, blanket, flask1, flask2, flask3, flask4, flask5, flask6, flask7, flask8, flask9, flask10, flask11, flask12, flask13, flask14, flask15, flask16, flask17, flask18, flask19, flask20, potion1, potion2, potion3, potion4, potion5, potion6, potion7, potion8, potion9, potion10, scroll1, scroll2, scroll3, scroll4, scroll5, scroll6, scroll7, scroll8, scroll9, scroll10, scroll11, scroll12, scroll13, jumpingPole, sharpeningTool, seeingItem, motionScanner, gorget, necklace1, necklace2, necklace3, necklace4, necklace5, necklace6, necklace7, necklace8, necklace9, imageItensifier, sightSharpening, ring1, ring2, ring3, ring4, ring5, ring6, ring7, ring8, armorLeather, armorMail, gloveFencing, gloveGauntlet, gloveJousting, buckler, shield, dagger, daggerDropBestWeapon, hammer, hammerParalyze, hammerSpark, sword, swordImpress, swordNullify, halberd, halberdPushActor, wand1, wand2, gem1, gem2, gem3, gem4, gem5, currency, explosive, firecracker :: ItemKind
 
 necklace, ring, potion, flask, scroll, wand, gem :: ItemKind  -- generic templates
 
@@ -294,26 +294,27 @@ blanket = ItemKind
 --
 -- No flask nor temporary organ of Calm depletion, since Calm reduced often.
 
-explosive = ItemKind -- Broadly based on the "flask" itemKind.
-{ isymbol = symbolFlask --Went with the general "thrown object" symbol.
-, iname = "explosive"
-, ifreq = [("useful", 100)]
-, iflavour = -- unsure how this works, but if it does work the way I think, the explosives will need new ones anyway.
-, icount = 1
-, irarity = [] -- unsure how this works.
-, iverbHit = "blast"
-, iweight = 500
-, idamage = toDmg 0
-, iaspects = []
-, ieffects = [Explode "blast 20"
-             , OnSmash (Explode "blast 20") ]]
-, ifeature = [Applicable, Lobable, toVelocity 50]
-, idesc = "The practical application of science."
-, ikit = []
+explosive = ItemKind  -- broadly based on the "flask" item kind
+  { isymbol = symbolFlask  -- went with the general "thrown object" symbol
+  , iname = "explosive"
+  , ifreq = [("useful", 100)]
+  , iflavour = zipPlain [BrRed]  -- unsure how this works, but if it does work the way I think, the explosives will need new ones anyway
+  , icount = 1
+  , irarity = []  -- unsure how this works
+  , iverbHit = "blast"
+  , iweight = 500
+  , idamage = toDmg 0
+  , iaspects = []
+  , ieffects = [ Explode "blast 20"
+               , OnSmash (Explode "blast 20") ]
+  , ifeature = [Applicable, Lobable, toVelocity 50]
+  , idesc = "The practical application of science."
+  , ikit = []
+  }
 
 firecracker = explosive
-{ idesc = "String and paper, concealing a deadly surprise."
-}
+  { idesc = "String and paper, concealing a deadly surprise."
+  }
 
 flask = ItemKind
   { isymbol  = symbolFlask
