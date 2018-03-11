54e12b3c7fc1115e933f712264df9175c7224d33
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2018-03-11 14:27:34 +0100

    Don't let the special gem dominate gem drops

--- GameDefinition/Content/ItemKind.hs
+++ GameDefinition/Content/ItemKind.hs
@@ -1449,7 +1449,7 @@ gem4 = gem1
 gem5 = gem1
   { isymbol  = symbolSpecial
   , iname    = "elixir"
-  , ifreq    = [ ("treasure", 100), ("gem", 100), ("any jewelry", 100)
+  , ifreq    = [ ("treasure", 100), ("gem", 25), ("any jewelry", 25)
                , ("valuable", 100) ]
   , iflavour = zipPlain [BrYellow]
   , irarity  = [(1, 40), (10, 40)]
