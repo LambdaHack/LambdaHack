0a319282c595ad47383aebebd626ca6c48054dac
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2018-04-08 10:33:47 +0200

    Hack around sparks from necklace getting in actor's way
    which may, unfortunately, still happen later on, after the actor
    is slowed down, etc.

--- Game/LambdaHack/Server/LoopM.hs
+++ Game/LambdaHack/Server/LoopM.hs
@@ -242,16 +242,16 @@ endClip updatePerFid = do
     execUpdAtomic $ UpdAgeGame arenas
     -- Perform periodic dungeon maintenance.
     when (clipN `mod` rleadLevelClips == 0) leadLevelSwitch
-    case clipN `mod` clipInTurn of
-      2 ->
-        -- Periodic activation only once per turn, for speed,
-        -- but on all active arenas. Calm updates and domination
-        -- happen there as well.
-        applyPeriodicLevel
-      4 ->
-        -- Add monsters each turn, not each clip.
-        unless (null arenas) spawnMonster
-      _ -> return ()
+    let clipMod = clipN `mod` clipInTurn
+    if | clipMod == clipInTurn - 1 ->
+         -- Periodic activation only once per turn, for speed,
+         -- but on all active arenas. Calm updates and domination
+         -- happen there as well.
+         applyPeriodicLevel
+       | clipMod == 2 ->
+         -- Add monsters each turn, not each clip.
+         unless (null arenas) spawnMonster
+       | otherwise -> return ()
   -- @applyPeriodicLevel@ might have, e.g., dominated actors, ending the game.
   -- It could not have unended the game, though.
   breakLoop2 <- getsServer sbreakLoop
