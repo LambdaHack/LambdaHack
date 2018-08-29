7bf720b4b81c2bf1703949ad70bc670c9e591eb1
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2018-08-29 18:09:53 +0200

    Fix spurious keybinding in the dashboard

--- GameDefinition/Client/UI/Content/Input.hs
+++ GameDefinition/Client/UI/Content/Input.hs
@@ -44,7 +44,8 @@ standardKeysAndMouse = InputContentRaw $ map evalKeyDef $
                , "cancel aiming/open main menu"
                , ByAimMode { exploration = ExecuteIfClear MainMenu
                            , aiming = Cancel } ))
-  , ("C-Escape", ([], "", MainMenu))  -- required by frontends; not shown
+  , ("C-Escape", ([CmdInternal], "", MainMenu))
+      -- required by frontends; not shown
   , ("Return", ( [CmdMinimal, CmdAim]
                , "accept target/open dashboard"
                , ByAimMode { exploration = ExecuteIfClear Dashboard
