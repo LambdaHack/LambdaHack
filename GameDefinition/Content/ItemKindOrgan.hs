bcbab93f8e045ed231c2b83414504e8c43d932d8
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2018-09-10 01:13:23 +0200

    Let sleep disable almost all skills

--- GameDefinition/Content/ItemKindOrgan.hs
+++ GameDefinition/Content/ItemKindOrgan.hs
@@ -461,10 +461,11 @@ asleep = armoredSkin
   , icount   = 5
   , iverbHit = "slay"
   , iweight  = 0
-  , iaspects = [ AddSkill SkMove (-1), AddSkill SkProject (-1)
-               , AddSkill SkSight (-3), AddSkill SkArmorMelee (-10)
-               , SetFlag Fragile, SetFlag Durable ]
-                   -- hack: display as condition
+  , iaspects = [AddSkill sk (-2) | sk <- [SkMove .. SkApply]]
+               ++ [ AddSkill SkMelee 2, AddSkill SkWait 2
+                  , AddSkill SkSight (-3), AddSkill SkArmorMelee (-10)
+                  , SetFlag Fragile, SetFlag Durable ]
+                      -- hack: display as condition
   , idesc    = "Sleep helps regain health, albeit extremely slowly. Being asleep makes an actor vulnerable, with gradually diminishing effects as the slumber wears off over several turns. Any non-idle action, not only combat but even yawning or stretching removes a sizable portion of the sleepiness."
   }
 impressed = armoredSkin
