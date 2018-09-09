adb1a47f3bab15481454cf08ecfdcf4dd83bdfe0
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2018-09-09 12:54:53 +0200

    Fix C-ESC not available to player

--- GameDefinition/Client/UI/Content/Input.hs
+++ GameDefinition/Client/UI/Content/Input.hs
@@ -44,7 +44,7 @@ standardKeysAndMouse = InputContentRaw $ map evalKeyDef $
                , "cancel aiming/open main menu"
                , ByAimMode { exploration = ExecuteIfClear MainMenu
                            , aiming = Cancel } ))
-  , ("C-Escape", ([CmdInternal], "", MainMenu))
+  , ("C-Escape", ([CmdNoHelp], "", MainMenu))
       -- required by frontends; not shown
   , ("Return", ( [CmdMinimal, CmdAim]
                , "accept target/open dashboard"
