170770400f7cdb916698ce19667f556ba8372010
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2018-11-02 19:56:40 +0100

    Tweak blast names to have better plurals

--- GameDefinition/Content/ItemKindBlast.hs
+++ GameDefinition/Content/ItemKindBlast.hs
@@ -398,7 +398,7 @@ distortion = ItemKind
   }
 smoke = ItemKind  -- when stuff burns out  -- unused
   { isymbol  = '`'
-  , iname    = "smoke"
+  , iname    = "smoke fume"  -- pluralizes better than 'smokes'
   , ifreq    = [("smoke", 1)]
   , iflavour = zipPlain [BrBlack]
   , icount   = 16
@@ -447,7 +447,7 @@ glue = ItemKind
   }
 waste = ItemKind
   { isymbol  = '*'
-  , iname    = "waste"
+  , iname    = "waste piece"
   , ifreq    = [("waste", 1)]
   , iflavour = zipPlain [Brown]
   , icount   = 16
