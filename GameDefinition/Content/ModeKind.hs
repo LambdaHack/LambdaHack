252676072fd2fedfbfe053976c90f8b45f88caf9
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2018-12-06 01:12:51 +0100

    Be less nasty and a bit less patronizing in game lost messages

--- GameDefinition/Content/ModeKind.hs
+++ GameDefinition/Content/ModeKind.hs
@@ -42,7 +42,7 @@ raid = ModeKind  -- mini-crawl
   , mcaves  = cavesRaid
   , mendMsg =
     let override =
-          [ (Defeated, "Sadly, the other team got the grant, while you were busy contemplating natural phenomena. Try to reason harder and more to the point next time.")
+          [ (Defeated, "Sadly, the other team snatched the grant, while you were busy contemplating natural phenomena. As sad as it sounds, there is competition in science. It's not enough to make a discovery, you have to be the first.")
           , (Escape, "You've got hold of the machine! Think of the hours of fun taking it apart and putting it together again! That's a great first step on your quest to solve the typing problems of the world.") ]
     in EM.fromList override `EM.union` genericEndMessages
   , mdesc   = "An incredibly advanced typing machine worth 100 gold is buried at the exit of this maze. Be the first to find it and fund a research team that makes typing accurate and dependable forever."
@@ -132,7 +132,7 @@ crawl = ModeKind
   , mcaves  = cavesCrawl
   , mendMsg =
     let override =
-          [ (Killed, "To think that followers of science and messengers of engligthment would earn death as their reward! Where did we err in our ways? Perhaps nature should not have been disturbed so brashly and the fell beast woken up from their slumber? Perhaps the gathered items should have been used for scientific experiments on the spot rather than hoarded as if of base covetousness? Or perhaps the challenge, chosen freely, but without the foreknowlege of the grisly difficulty, was insurmountable and lost from the very start, despite the enormous power of educated reason at out disposal?")
+          [ (Killed, "To think that followers of science and messengers of engligthment would earn death as their reward! Where did we err in our ways? Perhaps nature should not have been disturbed so brashly and the fell beasts woken up from their slumber? Perhaps the gathered items should have been used for scientific experiments on the spot rather than hoarded as if of base covetousness? Or perhaps the challenge, chosen freely, but without the foreknowlege of the grisly difficulty, was insurmountable and lost from the start, despite the enormous power of educated reason at out disposal?")
           , (Escape, "It's better to live to tell the tale than to choke on more than one can swallow. There was no more exquisite cultural artifacts and glorous scientific wonders in these forbidding tunnels anyway. Or was there?") ]
     in EM.fromList override `EM.union` genericEndMessages
   , mdesc   = "Enjoy the peaceful seclusion of these cold austere tunnels, but don't let wanton curiosity, greed and the ever-creeping abstraction madness keep you down there for too long. If you find survivors (whole or perturbed or segmented) of the past scientific missions, exercise extreme caution and engage or ignore at your discretion."
