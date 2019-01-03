71eeec7d1be1017824071d7d5e80d2baa78f2b62
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2019-01-03 15:40:42 +0100

    Bump release candidate revision number

--- Game/LambdaHack/Client/UI/HandleHumanGlobalM.hs
+++ Game/LambdaHack/Client/UI/HandleHumanGlobalM.hs
@@ -1286,9 +1286,9 @@ artWithVersion = do
         let exeVersion = rexeVersion corule
             libVersion = Self.version
             version = " Version " ++ showVersion exeVersion
-                      ++ "-rc7 (frontend: " ++ frontendName
+                      ++ "-rc8 (frontend: " ++ frontendName
                       ++ ", engine: LambdaHack " ++ showVersion libVersion
-                      ++ "-rc7) "
+                      ++ "-rc8) "
             versionLen = length version
             f line =
               let (prefix, versionSuffix) = T.breakOn "Version" line
