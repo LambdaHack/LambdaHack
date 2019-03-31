620430d0514ee6a7170b32c1728222620215587f
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2019-04-01 01:26:59 +0200

    Fix trying to make a projectile a leader if no our other actors alive
    but game not over (e.g., playing aliens in defence).

--- engine-src/Game/LambdaHack/Server/HandleEffectM.hs
+++ engine-src/Game/LambdaHack/Server/HandleEffectM.hs
@@ -126,7 +126,7 @@ refillHP source target speedDeltaHP = assert (speedDeltaHP /= 0) $ do
   when serious $ cutCalm target
   tb <- getsState $ getActorBody target
   fact <- getsState $ (EM.! bfid tb) . sfactionD
-  unless (fleaderMode (gplayer fact) == LeaderNull) $
+  unless (bproj tb || fleaderMode (gplayer fact) == LeaderNull) $
     -- If leader just lost all HP, change the leader early (not when destroying
     -- the actor), to let players rescue him, especially if he's slowed
     -- by the attackers.
