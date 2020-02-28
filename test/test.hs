d4c48bbd2faabf1c97ea4d5f65c1f2840dc39b18
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2020-02-28 01:11:36 +0100

    Port tests to tasty

--- test/test.hs
+++ test/test.hs
@@ -1,6 +1,8 @@
 import Prelude ()
 
 import Options.Applicative
+import Test.Tasty
+import Test.Tasty.HUnit
 
 import Game.LambdaHack.Client.UI.Frontend.Chosen
 import Game.LambdaHack.Core.Prelude
@@ -9,13 +11,22 @@ import Game.LambdaHack.Server
 import TieKnot
 
 main :: IO ()
-main = do
-  let args = words "--dbgMsgSer --logPriority 4 --newGame 1 --noAnim --maxFps 100000 --frontendNull --benchmark --stopAfterFrames 50 --automateAll --keepAutomated --gameMode crawl" ++ ["--setDungeonRng", "SMGen 123 123", "--setMainRng", "SMGen 123 123"]
-  serverOptions <- handleParseResult $ execParserPure defaultPrefs serverOptionsPI args
-  tieKnot serverOptions
-  when (frontendName == "sdl") $ do
-    -- The hacky log priority 0 tells SDL frontend to init and quit at once,
-    -- for testing on CIs without graphics access.
-    let args2 = words "--dbgMsgSer --logPriority 0 --newGame 3 --maxFps 100000 --benchmark --stopAfterFrames 50 --automateAll --keepAutomated --gameMode battle" ++ ["--setDungeonRng", "SMGen 125 125", "--setMainRng", "SMGen 125 125"]
-    serverOptions2 <- handleParseResult $ execParserPure defaultPrefs serverOptionsPI args2
-    tieKnot serverOptions2
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [integrationTests]
+
+integrationTests :: TestTree
+integrationTests = testGroup "integrationTests"
+  [ testCase "Null frontend; 50 frames" $ do
+      let args = words "--dbgMsgSer --logPriority 4 --newGame 1 --noAnim --maxFps 100000 --frontendNull --benchmark --stopAfterFrames 50 --automateAll --keepAutomated --gameMode crawl" ++ ["--setDungeonRng", "SMGen 123 123", "--setMainRng", "SMGen 123 123"]
+      serverOptions <- handleParseResult $ execParserPure defaultPrefs serverOptionsPI args
+      tieKnot serverOptions
+  , testCase "SDL fronted; init only" $
+      when (frontendName == "sdl") $ do
+        -- The hacky log priority 0 tells SDL frontend to init and quit at once,
+        -- for testing on CIs without graphics access.
+        let args2 = words "--dbgMsgSer --logPriority 0 --newGame 3 --maxFps 100000 --benchmark --stopAfterFrames 50 --automateAll --keepAutomated --gameMode battle" ++ ["--setDungeonRng", "SMGen 125 125", "--setMainRng", "SMGen 125 125"]
+        serverOptions2 <- handleParseResult $ execParserPure defaultPrefs serverOptionsPI args2
+        tieKnot serverOptions2
+  ]
