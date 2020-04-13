e504f6774dd7f137400736f0292150a3c5ffd9ad
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2020-04-13 15:39:54 +0200

    Slightly fix speed calculation to agree with what's on the wiki

--- engine-src/Game/LambdaHack/Common/Time.hs
+++ engine-src/Game/LambdaHack/Common/Time.hs
@@ -280,7 +280,7 @@ speedFromWeight !weight !throwVelocity =
   let w = fromIntegral weight
       mpMs | w < 250 = sInMs * 20
            | w < 1500 = sInMs * 20 * 1250 `div` (w + 1000)
-           | w < 10500 = sInMs * (11000 - w) `div` 1000
+           | w < 10500 = sInMs * (11500 - w) `div` 1000
            | otherwise = minimalSpeed * 2  -- move one step and drop
       v = mpMs * fromIntegral throwVelocity `div` 100
       -- We round down to the nearest multiple of 2M (unless the speed
