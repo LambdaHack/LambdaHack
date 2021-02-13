ecb2aead3c9d1ea7581a1e127ebd1c558fe3e56f
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2021-02-13 12:56:39 +0100

    Split manual into paragraphs aware of some markdown formatting

--- GameDefinition/game-src/Client/UI/Content/Screen.hs
+++ GameDefinition/game-src/Client/UI/Content/Screen.hs
@@ -31,11 +31,18 @@ standardLayoutAndFeatures = ScreenContent
         hGetContents handle
       let paragraphs :: [String] -> [String] -> [[String]]
           paragraphs [] rows = [reverse rows]
-          paragraphs (l : ls) rows = if null l
-                                     then reverse rows : paragraphs ls []
-                                     else paragraphs ls (l : rows)
+          paragraphs (l@"" : ls) rows = case (rows, ls) of
+            (('=':'=' : _) : _, _) ->  -- A title. No new paragraph.
+              paragraphs ls (l : rows)
+            (('-':'-' : _) : _, _) ->  -- A title. No new paragraph.
+              paragraphs ls (l : rows)
+            ((' ':' ':' ':' ' : _) : _, (' ':' ':' ':' ' : _) : _) ->
+              -- At least four spaces before and after; probably a code block.
+              paragraphs ls (l : rows)
+            _ -> reverse rows : paragraphs ls []
+          paragraphs (l : ls) rows = paragraphs ls (l : rows)
           intro = case paragraphs (lines x) [] of
-            _title : _blurb : par1 : par2 : _rest ->
+            _titleAndBlurb : par1 : par2 : _rest ->
               par1 ++ [""] ++ par2
             _ -> error "not enough paragraphs in intro screen text"
       lift intro)
