cb839ff86338b28f77c61d957a160aa972a38ebd
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2021-03-19 15:44:09 +0100

    Silence messages about dead actors activating most embeds

--- engine-src/Game/LambdaHack/Client/UI/DisplayAtomicM.hs
+++ engine-src/Game/LambdaHack/Client/UI/DisplayAtomicM.hs
@@ -1577,14 +1577,15 @@ displayRespSfxAtomicUI sfx = case sfx of
     itemD <- getsState sitemD
     let fid = bfid b
         isOurCharacter = fid == side && not (bproj b)
-        isOurAlive = isOurCharacter && bhp b > 0
+        isAlive = bhp b > 0
+        isOurAlive = isOurCharacter && isAlive
         isOurLeader = Just aid == mleader
         -- The message classes are close enough. It's melee or similar.
         feelLookHPBad bigAdj projAdj = do
           feelLook MsgBadMiscEvent MsgGoodMiscEvent bigAdj projAdj
           when isOurCharacter $ msgAdd MsgTutorialHint "You took damage of a different kind than the normal piercing hit, which means you armor didn't block any part of it. Normally, your HP (hit points, health) do not regenerate, so losing them is a big deal. Apply healing concoctions or take a long sleep to replenish your HP (but in this hectic environment not even uninterrupted resting that leads to sleep is easy)."
         feelLookHPGood = feelLook MsgGoodMiscEvent MsgBadMiscEvent
-        feelLookCalm bigAdj projAdj = when (bhp b > 0) $
+        feelLookCalm bigAdj projAdj = when isAlive $
           feelLook MsgEffectMinor MsgEffectMinor bigAdj projAdj
         -- Ignore @iid@, because it's usually obvious what item caused that
         -- and because the effects are not particularly disortienting.
@@ -1592,7 +1593,9 @@ displayRespSfxAtomicUI sfx = case sfx of
           let (verb, adjective) =
                 if bproj b
                 then ("get", projAdj)
-                else (if isOurCharacter then "feel" else "look", bigAdj)
+                else ( if isOurCharacter then "feel" else "look"
+                     , if isAlive then bigAdj else projAdj )
+                         -- dead body is an item, not a person
               msgClass = if | bproj b -> MsgEffectMinor
                             | isOurCharacter -> msgClassOur
                             | otherwise -> msgClassTheir
@@ -1613,10 +1616,12 @@ displayRespSfxAtomicUI sfx = case sfx of
         feelLookHPBad "wounded" "broken"
         let ps = (bpos b, bpos b)
         animate (blid b) $ twirlSplash ps Color.BrRed Color.Red
+      IK.RefillCalm{} | not isAlive -> return ()
       IK.RefillCalm{} | bproj b -> return ()
       IK.RefillCalm p | p == 1 -> return ()  -- no spam from regen items
       IK.RefillCalm p | p > 0 -> feelLookCalm "calmer" "stabilized"
       IK.RefillCalm _ -> feelLookCalm "agitated" "wobbly"
+      IK.Dominate | not isAlive -> return ()
       IK.Dominate -> do
         -- For subsequent messages use the proper name, never "you".
         let subject = partActor bUI
@@ -1651,11 +1656,14 @@ displayRespSfxAtomicUI sfx = case sfx of
               verb = "be now under"
           msgAdd MsgEffectMajor $ makeSentence
             [MU.SubjectVerbSg subject verb, MU.Text fidSourceName, "control"]
+      IK.Impress | not isAlive -> return ()
       IK.Impress -> aidVerbMU MsgEffectMinor aid "be awestruck"
+      IK.PutToSleep | not isAlive -> return ()
       IK.PutToSleep -> do
         let verb = "be put to sleep"
             msuffix = Just $ if fidSource == bfid b then "due to" else "by"
         mitemAidVerbMU MsgEffectMajor aid verb iid msuffix
+      IK.Yell | not isAlive -> return ()
       IK.Yell -> aidVerbMU MsgMiscellanous aid "start"
       IK.Summon grp p -> do
         let verbBase = if bproj b then "lure" else "summon"
@@ -1666,6 +1674,7 @@ displayRespSfxAtomicUI sfx = case sfx of
             verb = MU.Phrase [verbBase, object]
             msuffix = Just "with"
         mitemAidVerbMU MsgEffectMajor aid verb iid msuffix
+      IK.Ascend{} | not isAlive -> return ()
       IK.Ascend up -> do
         COps{cocave} <- getsState scops
         aidVerbMU MsgEffectMajor aid $ MU.Text $
@@ -1693,10 +1702,13 @@ displayRespSfxAtomicUI sfx = case sfx of
                  "The team joins" <+> makePhrase [partActor bUI]
                  <> ", forms a perimeter, repacks its belongings and leaves triumphant."
       IK.Escape{} -> return ()
+      IK.Paralyze{} | not isAlive -> return ()
       IK.Paralyze{} ->
         mitemAidVerbMU MsgEffectMedium aid "be paralyzed" iid (Just "with")
+      IK.ParalyzeInWater{} | not isAlive -> return ()
       IK.ParalyzeInWater{} ->
         aidVerbMU MsgEffectMinor aid "move with difficulty"
+      IK.InsertMove{} | not isAlive -> return ()
       IK.InsertMove d ->
         -- Usually self-inflicted of from embeds, so obvious, so no @iid@.
         if Dice.supDice d >= 10
@@ -1720,7 +1732,9 @@ displayRespSfxAtomicUI sfx = case sfx of
       IK.DropItem _ _ COrgan _ -> return ()
       IK.DropItem{} ->  -- rare enough
         mitemAidVerbMU MsgEffectMedium aid "be stripped" iid (Just "with")
+      IK.Recharge{} | not isAlive -> return ()
       IK.Recharge{} -> aidVerbMU MsgEffectMedium aid "heat up"
+      IK.Discharge{} | not isAlive -> return ()
       IK.Discharge{} -> aidVerbMU MsgEffectMedium aid "cool down"
       IK.PolyItem -> do
         subject <- partActorLeader aid
@@ -1784,8 +1798,11 @@ displayRespSfxAtomicUI sfx = case sfx of
         -- Don't make it modal if all info remains after no longer seen.
         unless (d `elem` [IK.DetectHidden, IK.DetectExit]) $
           displayMore ColorFull ""  -- the sentence short
+      IK.SendFlying{} | not isAlive -> return ()
       IK.SendFlying{} -> aidVerbMU MsgEffectMedium aid "be sent flying"
+      IK.PushActor{} | not isAlive -> return ()
       IK.PushActor{} -> aidVerbMU MsgEffectMedium aid "be pushed"
+      IK.PullActor{} | not isAlive -> return ()
       IK.PullActor{} -> aidVerbMU MsgEffectMedium aid "be pulled"
       IK.ApplyPerfume ->
         msgAdd MsgEffectMinor
@@ -1801,6 +1818,7 @@ displayRespSfxAtomicUI sfx = case sfx of
       IK.SeqEffect{} -> error $ "" `showFailure` sfx
       IK.When{} -> error $ "" `showFailure` sfx
       IK.IfThenElse{} -> error $ "" `showFailure` sfx
+      IK.VerbNoLonger{} | not isAlive -> return ()
       IK.VerbNoLonger verb ending -> do
         let msgClass = if fid == side
                        then MsgStatusStopUs
