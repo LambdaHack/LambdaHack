e743b2937323b8e3eaf331609349fa0374c64d5b
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2021-03-29 18:55:26 +0200

    Fix heroes starting on exit in escape scenario

--- GameDefinition/Content/CaveKind.hs
+++ GameDefinition/Content/CaveKind.hs
@@ -449,6 +449,7 @@ escape = rogue  -- a scenario with weak missiles, because heroes don't depend
   , cmaxStairsNum = 0
   , cescapeFreq   = [(OUTDOOR_ESCAPE_DOWN, 1)]
   , cstairFreq    = []
+  , cskip         = [0]  -- don't start heroes on exit
   , cdesc         = ""
   }
 zoo = rogue  -- few lights and many solids, to help the less numerous heroes
