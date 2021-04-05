88195e29afedba25a8b9e0489b19d7392ef894ab
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2021-04-05 19:44:05 +0200

    Add cabal.project.local for compiling production JS code

---
+++ cabal.project.local.js
@@ -0,0 +1,17 @@
+profiling: False
+library-profiling: False
+tests: False
+coverage: False
+library-coverage: False
+optimization: True
+documentation: False
+
+package *
+  documentation: False
+  library-profiling: False
+  profiling: False
+
+package LambdaHack
+  flags: +release -supportNodeJS
+
+
