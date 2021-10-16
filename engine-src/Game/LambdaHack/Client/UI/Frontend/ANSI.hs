9ba5a7bffa36d3c697223faee7b23459d5e7174f
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2021-10-16 16:57:13 +0200

    Add most standard non-modified keys to ANSI keyboard handling

--- engine-src/Game/LambdaHack/Client/UI/Frontend/ANSI.hs
+++ engine-src/Game/LambdaHack/Client/UI/Frontend/ANSI.hs
@@ -43,6 +43,7 @@ startup coscreen = do
               case c2 of
                 '\ESC' -> return [c]
                 '[' -> keycodeInput [c, c2]
+                '0' -> keycodeInput [c, c2]  -- not handled, but reported
                 _ -> return [c, c2]  -- Alt modifier
             else return [c]
           else return [c]
@@ -100,6 +101,11 @@ only bright foregrouns. And I have at least one bright backround: bright black.
   let Point{..} = PointArray.maxIndexByA (comparing Color.bgFromW32) singleArray
   ANSI.hSetCursorPosition SIO.stderr py px
   ANSI.hShowCursor SIO.stderr
+  -- Do not trash people's terminals when interrupted:
+  ANSI.hSetSGR SIO.stderr [uncurry (ANSI.SetColor ANSI.Foreground)
+                           $ colorTranslate Color.White]
+  ANSI.hSetSGR SIO.stderr [uncurry (ANSI.SetColor ANSI.Background)
+                           $ colorTranslate Color.Black]
   SIO.hFlush SIO.stderr
 
 -- This is contrived, because we don't want to depend on libraries
@@ -118,18 +124,87 @@ keyTranslate e = (\(key, modifier) -> K.KM modifier key) $
     [c] | ord '\^A' <= ord c && ord c <= ord '\^Z' ->
           -- Alas, only lower-case letters.
           (K.Char $ chr $ ord c - ord '\^A' + ord 'a', K.Control)
-        | -- Movement keys are more important than leader picking,
-          -- so disabling the latter and interpreting the keypad numbers
-          -- as movement:
+        | -- On (some) terminal emulators Shift-keypad direction produces
+          -- the same code as number keys. A sensible workaround for that
+          -- is using Control for running, but it's not clear how portable
+          -- this is, so we do not rely on this exclusively. Since movement
+          -- keys are more important than leader picking, we are disabling
+          -- the latter and interpreting the keypad numbers as movement.
+          --
+          -- BTW, S-KP_5 and C-KP_5 are still not correctly handled
+          -- on some terminals, so this may be the biggest portability problem.
           c `elem` ['1'..'9'] -> (K.KP c, K.NoModifier)
         | otherwise           -> (K.Char c, K.NoModifier)
     _ -> (K.Unknown e, K.NoModifier)
 
+-- From https://en.wikipedia.org/wiki/ANSI_escape_code#Terminal_input_sequences
 keycodeTranslate :: String -> (K.Key, K.Modifier)
 keycodeTranslate e =
   case e of
     "1~" -> (K.Home, K.NoModifier)
+    "2~" -> (K.Insert, K.NoModifier)
+    "3~" -> (K.Delete, K.NoModifier)
+    "4~" -> (K.End , K.NoModifier)
+    "5~" -> (K.PgUp, K.NoModifier)
+    "6~" -> (K.PgDn, K.NoModifier)
+    "7~" -> (K.Home, K.NoModifier)
+    "8~" -> (K.End, K.NoModifier)
+    "9~" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "10~" -> (K.Fun 0, K.NoModifier)
+    "11~" -> (K.Fun 1, K.NoModifier)
+    "12~" -> (K.Fun 2, K.NoModifier)
+    "13~" -> (K.Fun 3, K.NoModifier)
+    "14~" -> (K.Fun 4, K.NoModifier)
+    "15~" -> (K.Fun 5, K.NoModifier)
+    "17~" -> (K.Fun 6, K.NoModifier)
+    "18~" -> (K.Fun 7, K.NoModifier)
+    "19~" -> (K.Fun 8, K.NoModifier)
+    "20~" -> (K.Fun 9, K.NoModifier)
+    "21~" -> (K.Fun 10, K.NoModifier)
+    "22~" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "23~" -> (K.Fun 11, K.NoModifier)
+    "24~" -> (K.Fun 12, K.NoModifier)
+    "25~" -> (K.Fun 13, K.NoModifier)
+    "26~" -> (K.Fun 14, K.NoModifier)
+    "27~" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "28~" -> (K.Fun 15, K.NoModifier)
+    "29~" -> (K.Fun 16, K.NoModifier)
+    "30~" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "32~" -> (K.Fun 18 , K.NoModifier)
+    "33~" -> (K.Fun 19 , K.NoModifier)
+    "34~" -> (K.Fun 20 , K.NoModifier)
+    "35~" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+
+    "A" -> (K.Up, K.NoModifier)
+    "B" -> (K.Down, K.NoModifier)
+    "C" -> (K.Right, K.NoModifier)
+    "D" -> (K.Left, K.NoModifier)
+    "E" -> (K.Begin, K.NoModifier)
+    "F" -> (K.End, K.NoModifier)
+    "G" -> (K.KP '5', K.NoModifier)
     "H" -> (K.Home, K.NoModifier)
+    "I" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "J" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "K" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "L" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "M" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "N" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "O" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "1P" -> (K.Fun 1, K.NoModifier)
+    "1Q" -> (K.Fun 2, K.NoModifier)
+    "1R" -> (K.Fun 3, K.NoModifier)
+    "1S" -> (K.Fun 4, K.NoModifier)
+    "T" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "U" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "V" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "W" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "X" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "Y" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+    "Z" -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
+
+--    "r" -> (K.Begin, K.NoModifier)
+--    "u" -> (K.Begin, K.NoModifier)
+
     _ -> (K.Unknown $ "\\ESC[" ++ e, K.NoModifier)
 
 setAttr :: Color.Attr -> (Color.Color, Color.Color)
